d8b620e2ecb4864f9bf5a525b1c5415218c14152
Locke <locke@Lockes-MacBook-Pro.local>
2022-07-31 16:44:07 -0700

    mvp for mint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,30 @@ import '../styles/globals.css'
 import Head from 'next/head'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.localhost, chain.rinkeby],
+  [
+    jsonRpcProvider({ rpc: () => ({ http: 'https://rpc.ankr.com/eth' }) }),
+    publicProvider()
+  ]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: connectors,
+  provider: provider
+})
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   return (
@@ -21,11 +45,15 @@ function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
           src="https://www.googletagmanager.com/gtag/js?id=G-25NZ8J2L82"
         ></script>
       </Head>
-      <Header />
-      <div className="pt-[78px]">
-        <Component {...pageProps} />
-      </div>
-      <Footer />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Header />
+          <div className="pt-[78px]">
+            <Component {...pageProps} />
+          </div>
+          <Footer />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </div>
   )
 }
