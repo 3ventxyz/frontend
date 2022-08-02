a83ca400d14eed95615545e1209d64c1567bb349
Aline Ayumi <aline.ayumi@unico.io>
2022-08-01 23:46:54 -0300

    feat: adding firebase configutarion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import '../styles/globals.css'
 import Head from 'next/head'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="h-screen w-screen">
       <Head>
@@ -12,14 +13,8 @@ function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
           name="description"
           content="your web3 events and ticketing solution"
         />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.svg" />
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-25NZ8J2L82"
-        ></script>
       </Head>
       <Header />
       <div className="pt-[78px]">
