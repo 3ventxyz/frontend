d8e3000b92b600146bfb02fd615fb8cc56902029
marrodri <laza.mar64@gmail.com>
2022-08-04 11:22:08 -0700

    firestore lite removed; shared interface added; dashboard page file structure added

---
+++ pages/details/[eid].tsx
@@ -0,0 +1,4 @@
+
+
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
---
+++ pages/shared/interface/common.ts
@@ -0,0 +1,23 @@
+export interface TicketInterface {
+  ticketTitle: string
+  registeredUsers: string
+  tokenId: string
+  price: string
+  disabled?: boolean
+  isSelected?: boolean
+}
+
+export interface EventInterface {
+  id: string
+  eventTitle: string
+  orgTitle: string
+  date: string
+  address: string
+  imgURL: string
+  tickets?: TicketInterface[]
+}
+
+export interface UserProfileEvents {
+  upcomingEvents: EventInterface[]
+  pastEvents: EventInterface[]
+}
