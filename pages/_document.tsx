750766a397d6e8a83c669ffda9ae571d6ea9b4e8
Aline Ayumi <aline.ayumi@unico.io>
2022-08-04 22:04:50 -0300

    fix: Firebase configuration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_FIREBASE_MEASUREMENT_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
         />
         <script
           dangerouslySetInnerHTML={{
@@ -16,7 +16,7 @@ export default function Document() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_FIREBASE_MEASUREMENT_ID}', {
+            gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', {
               page_path: window.location.pathname,
             });
           `
