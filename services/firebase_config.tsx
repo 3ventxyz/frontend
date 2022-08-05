0cf61a5e9d305241dcb2f83a8d04f69afaf2f969
Aline Ayumi Fukuhara <61579519+alineayumi@users.noreply.github.com>
2022-08-05 00:03:35 -0300

    fix: Firebase configuration (#7)

--- services/firebase_config.tsx
+++ services/firebase_config.tsx
@@ -1,16 +1,16 @@
 import { initializeApp } from '@firebase/app'
 import { getAnalytics } from '@firebase/analytics'
-import { getFirestore } from '@firebase/firestore/lite'
+import { getFirestore } from '@firebase/firestore'
 import { getAuth } from '@firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_API_KEY_DEV,
-  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN_DEV,
-  projectId: process.env.NEXT_FIREBASE_PROJECT_ID_DEV,
-  storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET_DEV,
-  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID_DEV,
-  appId: process.env.NEXT_FIREBASE_APP_ID_DEV,
-  measurementId: process.env.NEXT_FIREBASE_MEASUREMENT_ID_DEV
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
