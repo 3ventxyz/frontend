32d70bc0b422c4c6f8c845a44e6c53d45a2d7e68
Locke <locke@Lockes-MacBook-Pro.local>
2022-08-06 11:11:00 -0700

    raise shared to root level in project

--- services/firebase_config.tsx
+++ services/firebase_config.tsx
@@ -13,9 +13,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
-console.log(firebaseConfig)
-console.log(process.env)
-
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 // const analytics = getAnalytics(app)
