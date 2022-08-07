768196f03475b8a806f0ee5bfa38248ab3bfbca5
Aline Ayumi <aline.ayumi@unico.io>
2022-08-07 00:14:44 -0300

    feat: button with type +  Layout component with min height screen

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -32,55 +32,51 @@ export default function Login() {
   }
 
   return (
-    <section className="h-full w-full bg-secondaryBg">
-      <div className="p-hor sm:p-horSm flex flex-col items-center justify-center lg:grid lg:grid-cols-9">
-        <div className="flex w-full flex-col  justify-center px-4 lg:col-span-4">
-          <form action="" onSubmit={handleSubmit}>
-            {/* email input */}
-            <div className="mb-6">
-              <input
-                type="text"
-                className="text-primaryDark focus:text-secondaryDark bg-surfaceDark border-backgroundDark focus:border-tertiaryDark form-control m-0 block w-full rounded border border-solid bg-clip-padding py-2 px-4 text-xl font-normal transition ease-in-out focus:outline-none"
-                placeholder="Email address"
-                ref={emailRef}
-                required
-              />
-            </div>
-            {/* password input */}
-            <div className="mb-6">
-              <input
-                type="password"
-                className="text-primaryDark focus:text-secondaryDark bg-surfaceDark border-backgroundDark focus:border-tertiaryDark form-control m-0 block w-full rounded border border-solid bg-clip-padding py-2 px-4 text-xl font-normal transition ease-in-out focus:outline-none"
-                placeholder="Password"
-                ref={passwordRef}
-                required
-              />
-            </div>
-            <div className="mb-6 flex items-center justify-between">
-              <a
-                href="#!"
-                className="text-secondaryDark hover:text-tertiaryDark focus:text-primaryDark active:text-primaryDark transition duration-200 ease-in-out"
-              >
-                Forgot password?
-              </a>
-            </div>
-            <Button
-              text="LOGIN"
-              active={!loading}
-              onClick={() => null}
-              type="submit"
-            />
-            <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-gray-300 after:mt-0.5 after:flex-1 after:border-t after:border-gray-300">
-              <p className="text-primaryDark mx-4 mb-0 text-center font-semibold">
-                Don&apos;t have an account?
-              </p>
-            </div>
-            <Link href="/register">
-              <a>Register</a>
-            </Link>
-          </form>
+    <div className="p-auto flex flex-grow items-center justify-center bg-secondaryBg">
+      <form action="" onSubmit={handleSubmit}>
+        {/* email input */}
+        <div className="mb-6">
+          <input
+            type="text"
+            className="text-primaryDark focus:text-secondaryDark bg-surfaceDark border-backgroundDark focus:border-tertiaryDark form-control m-0 block w-full rounded border border-solid bg-clip-padding py-2 px-4 text-xl font-normal transition ease-in-out focus:outline-none"
+            placeholder="Email address"
+            ref={emailRef}
+            required
+          />
         </div>
-      </div>
-    </section>
+        {/* password input */}
+        <div className="mb-6">
+          <input
+            type="password"
+            className="text-primaryDark focus:text-secondaryDark bg-surfaceDark border-backgroundDark focus:border-tertiaryDark form-control m-0 block w-full rounded border border-solid bg-clip-padding py-2 px-4 text-xl font-normal transition ease-in-out focus:outline-none"
+            placeholder="Password"
+            ref={passwordRef}
+            required
+          />
+        </div>
+        <div className="mb-6 flex items-center justify-between">
+          <a
+            href="#!"
+            className="text-secondaryDark hover:text-tertiaryDark focus:text-primaryDark active:text-primaryDark transition duration-200 ease-in-out"
+          >
+            Forgot password?
+          </a>
+        </div>
+        <Button
+          text="LOGIN"
+          active={!loading}
+          onClick={() => null}
+          type="submit"
+        />
+        <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-gray-300 after:mt-0.5 after:flex-1 after:border-t after:border-gray-300">
+          <p className="text-primaryDark mx-4 mb-0 text-center font-semibold">
+            Don&apos;t have an account?
+          </p>
+        </div>
+        <Link href="/register">
+          <a>Register</a>
+        </Link>
+      </form>
+    </div>
   )
 }
