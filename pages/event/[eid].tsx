4ca78ef6ac5b3a819f912cbb27d97460b1ff34b4
marrodri <laza.mar64@gmail.com>
2022-08-08 16:43:12 -0700

    event page: event details show up and select ticket button layout fix

--- pages/event/[eid].tsx
+++ pages/event/[eid].tsx
@@ -1,8 +1,17 @@
+import {
+  collection,
+  doc,
+  DocumentData,
+  DocumentSnapshot,
+  getDoc
+} from '@firebase/firestore'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import TicketButton from '../../components/ticketButton'
+import { db } from '../../services/firebase_config'
 import { TicketInterface } from '../../shared/interface/common'
 import { EventInterface } from '../../shared/interface/common'
+import Image from 'next/image'
 
 export default function Event() {
   // TODO: fetch event data
@@ -17,10 +26,27 @@ export default function Event() {
   const { eid } = router.query
 
   useEffect(() => {
+    const newEventData = (eventDoc: DocumentSnapshot<DocumentData>) => {
+      const eventData: EventInterface = {
+        address: eventDoc.data()?.address,
+        date: eventDoc.data()?.date,
+        id: eventDoc.data()?.id,
+        eventTitle: eventDoc.data()?.eventTitle,
+        orgTitle: eventDoc.data()?.orgTitle,
+        imgURL: eventDoc.data()?.imgURL
+      }
+      return eventData
+    }
     const fetchData = async () => {
       //get and set the data from firebase
       //get EventInterface for the correct eventID
-      setEvent(undefined)
+      const eventId: any = eid
+      console.log('============fetchData::eid============')
+      console.log(eid)
+      console.log('=================================')
+      const docRef = doc(db, 'events', eventId)
+      const eventDoc = await getDoc(docRef)
+      setEvent(newEventData(eventDoc))
       setFetched(true)
     }
     if (!fetched) {
@@ -38,58 +64,77 @@ export default function Event() {
   }
 
   return (
-    <div className="flex flex-row space-x-[291px] bg-dashboard px-[210px] pt-[85px] pb-[106px]">
+    <div className="bg-dashboard flex flex-row space-x-[291px] px-[210px] pt-[85px] pb-[106px]">
       {/* TODO build the event page, after a user clicks the button 'create event' */}
-      <div className="flex h-full flex-col">
-        <div
-          id="event-details"
-          className="mb-[50px] space-y-[20px] text-[14px] font-medium leading-[19px]"
-        >
-          <h3>Event Title</h3>
-          <div>
-            Date and Time <br /> Address
-          </div>
-
-          <div className="h-[100px] w-[100px] rounded-[20px] bg-green-100">
-            {' '}
-            google map
-          </div>
-          <div>description of the event</div>
-        </div>
-        <div
-          id="ticketbuilder"
-          className="flex w-[373px] flex-col items-center space-y-[19px]"
-        >
-          {TicketListData.map((ticket: TicketInterface, index) => {
-            return (
-              <div key={ticket.tokenId}>
-                <button
-                  className="w-full"
-                  onClick={() => {
-                    setSelectedTicket(ticket)
-                  }}
-                >
-                  <TicketButton ticket={ticket} />
-                </button>
+      {fetched ? (
+        <>
+          <div className="flex h-full flex-col">
+            <div
+              id="event-details"
+              className="mb-[50px] space-y-[20px] text-[14px] font-medium leading-[19px]"
+            >
+              <h3>
+                {event?.eventTitle !== null ? event?.eventTitle : 'Event Title'}
+              </h3>
+              <div>
+                {event?.date} <br />
+                {event?.address}
               </div>
-            )
-          })}
-          <button
-            onClick={() => {
-              purchaseSelectedTier()
-            }}
-          >
-            <div className="h-[40px]  w-[97px] rounded-xl bg-white px-[20px] py-[10px] text-[14px] font-semibold">
-              Register
+
+              <div className="relative h-[100px] w-[100px] rounded-[20px] bg-green-100">
+                <Image
+                  src={`https://maps.googleapis.com/maps/api/staticmap?center=City+Hall,New+York,NY&zoom=15&size=205x205&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}`}
+                  layout='fill'
+                  loading='lazy'
+                  objectFit='cover'
+                  className='rounded-[20px]'
+                />
+              </div>
+              <div>description of the event</div>
             </div>
-          </button>
-        </div>
-      </div>
-      <div className="h-full">
-        <div className="h-[400px] w-[400px] rounded-[67px] bg-slate-400 px-[50px] py-[50px]">
-          event img
-        </div>
-      </div>
+            <div
+              id="ticketbuilder"
+              className="flex w-[373px] flex-col items-center space-y-[19px]"
+            >
+              {TicketListData.map((ticket: TicketInterface, index) => {
+                return (
+                  <button
+                    key={ticket.tokenId}
+                    className="w-full"
+                    onClick={() => {
+                      setSelectedTicket(ticket)
+                    }}
+                  >
+                    <TicketButton ticket={ticket} />
+                  </button>
+                )
+              })}
+              <button
+                onClick={() => {
+                  purchaseSelectedTier()
+                }}
+              >
+                <div className="h-[40px]  w-[97px] rounded-xl bg-white px-[20px] py-[10px] text-[14px] font-semibold">
+                  Register
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className="h-full">
+            <div className="h-[400px] w-[400px] relative rounded-[67px] bg-slate-400 px-[50px] py-[50px]">
+              <Image
+                  src={event ? event.imgURL: ''}
+                  layout='fill'
+                  loading='lazy'
+                  objectFit='cover'
+                  className='rounded-[20px]'
+                />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>Loading</div>
+      )}
     </div>
   )
 }
