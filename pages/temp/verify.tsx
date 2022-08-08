a4cb0d6960ffbec91ed9c7fcee5a401afff60f1b
constanzadl <constanzadl94@gmail.com>
2022-08-08 18:36:15 -0500

    discord oauth2 database check

--- pages/temp/verify.tsx
+++ pages/temp/verify.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useRouter } from 'next/router'
 import DiscordConfig from "../../services/discord_config";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../../services/firebase_config';
 
 export default function Verify() {
     const { asPath } = useRouter()
     const [accessCode, setAccessCode] = useState('')
+    const [discordVerified, setDiscordVerified] = useState(false)
 
     useEffect(() => {
         const pathParts = asPath.split('=')
@@ -13,24 +16,35 @@ export default function Verify() {
             setAccessCode(hash)
             /*Use access code once app was authorized*/
             if (hash !== '') {
-              console.log(`Access Code: ${hash}`)
               /*Get access token from discord_config*/
-              DiscordConfig(hash)
-            }         
+                DiscordConfig(hash, setDiscordVerified)
+            }
         } else {
           setAccessCode('')
         }
-    }, [asPath])
+    }, [])
 
-    return (
-    <div className="px-32">
-        <h3 className="w-full max-w-[480px]">Verify your account</h3>
-        <div className="py-4">
-            <a href="https://discord.com/api/oauth2/authorize?client_id=997585077548617728&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Ftemp%2Fverify&response_type=code&scope=identify" className="h-[40px] w-fit items-center justify-center rounded-[6px] px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white bg-[#5865f2] hover:bg-[#4752c4]">
-                Discord
-            </a>
-    </div>
-    </div>
-    )
-
-}
+    if(discordVerified) {
+        console.log('verified!')
+        return (
+            <div className="px-32">
+                <h3 className="w-full max-w-[480px]">Verify your account</h3>
+                <div className="py-4">
+                    <p className="h-[40px] w-fit items-center justify-center rounded-[6px] px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white bg-[#5865f2] hover:bg-[#4752c4]">
+                        Discord Verified
+                    </p>
+            </div>
+            </div>
+            )
+    }
+        return (
+        <div className="px-32">
+            <h3 className="w-full max-w-[480px]">Verify your account</h3>
+            <div className="py-4">
+                <a href={process.env.NEXT_PUBLIC_DISCORD_OAUTH_URL} className="h-[40px] w-fit items-center justify-center rounded-[6px] px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white bg-[#5865f2] hover:bg-[#4752c4]">
+                    Discord
+                </a>
+        </div>
+        </div>
+        )
+    }
--- services/discord_config.tsx
+++ services/discord_config.tsx
@@ -1,18 +1,19 @@
 import axios from 'axios'
-import { app } from './firebase_config';
+import { app, db } from './firebase_config';
 import url from 'url'
+import { doc, updateDoc, collection, getDoc } from 'firebase/firestore'
 
-export default async function DiscordConfig (accessCode: string){
+export default async function DiscordConfig (accessCode: string, setDiscordVerified: any ){
     // TODO: env variables
     try {
         const url = 'https://discord.com/api/v10/oauth2/token'
 
         const formData = new URLSearchParams({
-            "client_id": '997585077548617728',
-            "client_secret": 'e-LTOH-gMZfdIZXCq16EdrQR-AvAR7Qv', 
+            "client_id": process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID.toString(),
+            "client_secret": process.env.NEXT_PUBLIC_DISCORD_API_SECRET.toString(), 
             "grant_type": 'authorization_code',
             "code": accessCode,
-            "redirect_uri": 'http://localhost:3000/temp/verify',
+            "redirect_uri": process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URL.toString(),
             "scope": 'identify'
         });
 
@@ -23,11 +24,39 @@ export default async function DiscordConfig (accessCode: string){
             },
             body: formData
         })
+        let data = await response.json();
+        data = JSON.stringify(data);
+        const token = JSON.parse(data).access_token;
+        if (token && token !== undefined) {
+            /*change value on database*/
+                try {
+                  const docRef = doc(db, 'user', 'guJqAglqTLAzoMIQA6Gi');
+                  await updateDoc(docRef, {
+                    discordVerified: true
+                  })
+                  console.log('Data written into doc ID: ', docRef.id)
+                } catch (e) {
+                  console.error('Error adding data: ', e)
+                }
+              }
+                } catch(err) {
+                    console.log(err);
+                }
 
+    /*Check database to change FE*/
+    const docRef = doc(db, "user", "guJqAglqTLAzoMIQA6Gi");
+    const docSnap = await getDoc(docRef);
 
-        console.log(response)
-        console.log(response.json())
-    } catch(err) {
-        console.log(err);
+    if (docSnap.exists()) {
+        if(docSnap.data().discordVerified === true){
+            setDiscordVerified(true)
+            console.log('verification succeded')
+        }
+        else
+            console.log('verification failed')
+    console.log("Document data:", docSnap.data());
+    } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
     }
 }
