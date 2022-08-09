3bf71637110b01d6a85ff520659d171256cc7c6d
marrodri <laza.mar64@gmail.com>
2022-08-09 15:20:45 -0700

    event page ticket flow modal component and basic logic added

---
+++ components/modal.tsx
@@ -0,0 +1,23 @@
+export default function Modal({
+  visible,
+  onClose
+}: {
+  visible: boolean
+  onClose: () => void
+}) {
+  const handleOnClose = (e: any) => {
+    if (e.target.id === 'container') {
+      onClose()
+    }
+  }
+  if (!visible) return null
+  return (
+    <div
+      id="container"
+      onClick={handleOnClose}
+      className="backdrop-blue-sm fixed inset-0 flex items-center justify-center bg-black bg-opacity-25"
+    >
+      <div className="w-72 rounded bg-white p-2">modal children</div>
+    </div>
+  )
+}
