754398c7d85460347ecf450c83af8c776820af9c
marrodri <laza.mar64@gmail.com>
2022-08-10 19:36:35 -0700

    dashboard-page::see-all; toggle between upcoming and past events added

--- components/button.tsx
+++ components/button.tsx
@@ -2,21 +2,42 @@ export default function Button({
   text,
   onClick,
   active,
-  type = 'button'
+  type = 'button',
+  activeStyling = false
 }: {
   text: string
   onClick: () => void
   active: boolean
   type?: 'button' | 'submit' | 'reset' | undefined
+  activeStyling?: boolean
 }) {
-  return (
-    <button
-      type={type}
-      disabled={!active}
-      onClick={onClick}
-      className="h-[40px] w-fit items-center justify-center rounded-[6px] bg-primary px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white"
-    >
-      {text}
-    </button>
-  )
+  if (!activeStyling) {
+    return (
+      <button
+        type={type}
+        disabled={!active}
+        onClick={onClick}
+        className="h-[40px] w-fit items-center justify-center rounded-[6px] bg-primary px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white"
+      >
+        {text}
+      </button>
+    )
+  } else {
+    return (
+      <button
+        type={type}
+        disabled={!active}
+        onClick={onClick}
+        className={`
+      ${
+        !active
+          ? 'bg-black text-white'
+          : 'bg-white text-disabled hover:bg-black hover:text-white'
+      }
+      h-[40px] w-fit cursor-pointer items-center justify-center rounded-[6px] border px-[20px] py-[10px] text-[14px]  font-semibold`}
+      >
+        {text}
+      </button>
+    )
+  }
 }
--- pages/dashboard/seeAll.tsx
+++ pages/dashboard/seeAll.tsx
@@ -6,22 +6,36 @@ import { useEvents } from '../../context/eventsContext'
 import { db } from '../../services/firebase_config'
 import { EventInterface } from '../../shared/interface/common'
 import EventsDisplay from './components/eventsDisplay'
+import { HiChevronLeft } from 'react-icons/hi'
+import Button from '../../components/button'
 
 export default function SeeAll() {
   const router = useRouter()
-  const eventsCont = useEvents()
+  const events = useEvents()
   const [title, setTitle] = useState('')
-  const [fetched, setIsFetched] = useState(false)
+  // const [fetched, setIsFetched] = useState(false)
   const [eventsData, setEvents] = useState<Array<EventInterface>>([])
-  const { events } = router.query
+  const [mode, setMode] = useState('upcoming')
+
   useEffect(() => {
-    const fetchData = async () => {
+    if (typeof router.query.events === 'object') {
+      setMode('upcoming')
+    } else {
+      setMode(router.query.events || 'upcoming')
+    }
+  }, [router.query.events])
+
+  useEffect(() => {
+    const setData = async () => {
+      // if(!events) return
+
       let eventData: Array<EventInterface>
       const userDocRef = doc(db, 'user', 'guJqAglqTLAzoMIQA6Gi')
-      switch (events) {
+
+      switch (mode) {
         case 'upcoming':
           setTitle('all upcoming events')
-          eventData = await eventsCont.fetchEventsData({
+          eventData = await events.fetchEventsData({
             collectionRef: collection(userDocRef, 'upcomingEvents'),
             numberOfEvents: 6
           })
@@ -29,7 +43,7 @@ export default function SeeAll() {
           break
         case 'past':
           setTitle('all past events')
-          eventData = await eventsCont.fetchEventsData({
+          eventData = await events.fetchEventsData({
             collectionRef: collection(userDocRef, 'pastEvents'),
             numberOfEvents: 6
           })
@@ -39,24 +53,48 @@ export default function SeeAll() {
           console.log('no setting error 404')
           break
       }
-      setIsFetched(true)
     }
-    if (!fetched && events) {
-      fetchData()
-    }
-  }, [events])
+    setData()
+  }, [mode])
+
   return (
-    <div className="flex flex-col bg-secondaryBg px-[20px] pb-[106px] pt-[35px] md:px-[112px]">
-      {fetched ? (
-        <EventsDisplay
-          title={title}
-          route={''}
-          query={undefined}
-          eventsData={eventsData}
-        ></EventsDisplay>
-      ) : (
-        <>loading...</>
-      )}
+    <div className="flex flex-col items-center bg-secondaryBg px-[20px] pb-[106px] pt-[35px] md:px-[112px]">
+      {/* <div className="flex flex-col bg-secondaryBg px-[20px] pb-[106px] pt-[35px] md:px-[112px]"> */}
+      <div className="mx-auto mb-[20px] flex w-full max-w-[1200px] flex-row items-center justify-between border-b border-disabled pb-2">
+        <div className="flex flex-row items-center">
+          <button
+            className="h-[40px]"
+            onClick={() => {
+              router.back()
+            }}
+          >
+            <HiChevronLeft className="h-full" />
+          </button>
+          <p className="text-[25px] font-bold md:text-[32px]">{title}</p>
+        </div>
+        <div className="flex flex-row space-x-4">
+          <Button
+            text="upcoming"
+            onClick={() => setMode('upcoming')}
+            active={mode !== 'upcoming'}
+            activeStyling={true}
+          />
+          <Button
+            text="past"
+            onClick={() => setMode('past')}
+            active={mode !== 'past'}
+            activeStyling={true}
+          />
+        </div>
+      </div>
+
+      <EventsDisplay
+        title={title}
+        route={''}
+        query={undefined}
+        eventsData={eventsData}
+        showHeader={false}
+      />
     </div>
   )
 }
