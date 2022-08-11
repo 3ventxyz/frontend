c6961a52d7ba7345b2bfcad39b05ee44633197a0
constanzadl <constanzadl94@gmail.com>
2022-08-10 19:08:53 -0500

    twitter verification

--- services/verify_profile.tsx
+++ services/verify_profile.tsx
@@ -44,3 +44,48 @@ export async function VerifyDiscord(
     console.log(err)
   }
 }
+
+export async function VerifyTwitter(
+  accessCode: string,
+  uid: string
+) {
+  try {
+    const url = 'https://api.twitter.com/2/oauth2/token'
+
+    const formData = new URLSearchParams({
+      code: accessCode,
+      grant_type: 'authorization_code',
+      client_id: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID || '',
+      redirect_uri: process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URL || '',
+      code_verifier: 'challenge'
+    })
+
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': 'http://localhost:3000'
+      },
+      body: formData
+    })
+    let data = await response.json()
+    console.log(data)
+    data = JSON.stringify(data)
+    const token = JSON.parse(data).access_token
+    if (token && token !== undefined) {
+      /*change value on database*/
+      try {
+        const docRef = doc(db, 'user', uid)
+        await updateDoc(docRef, {
+          twitterVerified: true
+        })
+        console.log('Data written into doc ID: ', docRef.id)
+      } catch (e) {
+        console.error('Error adding data: ', e)
+      }
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
