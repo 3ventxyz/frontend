b9ccaa8670d946de54f5d22a60d56bb1cd089266
marrodri <laza.mar64@gmail.com>
2022-08-12 17:44:05 -0700

    spinner and textInput moved to global components

---
+++ components/spinner.tsx
@@ -0,0 +1,10 @@
+export default function Spinner() {
+  return (
+    <div
+      className="h-[100px] w-[100px] animate-spin rounded-full border-4 border-solid border-blue-400 border-t-white/[.00]"
+      role="status"
+    >
+      <span className="hidden">Loading...</span>
+    </div>
+  )
+}
