6e6d4cab2bcf9d93a30187cae358accd97f62fe9
constanzadl <constanzadl94@gmail.com>
2022-08-14 15:36:05 -0500

    url path completed

--- pages/verify/index.tsx
+++ pages/verify/index.tsx
@@ -3,13 +3,15 @@ import { useRouter } from 'next/router'
 import { VerifyDiscord, VerifyTwitter } from '../../services/verify_profile'
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../../services/firebase_config'
+import absoluteUrl from 'next-absolute-url'
+import { getRedirectResult } from '@firebase/auth'
 
 export default function Verify() {
   const { asPath } = useRouter()
   const router = useRouter()
-
   const [discordVerified, setDiscordVerified] = useState(false)
   const [twitterVerified, setTwitterVerified] = useState(false)
+  const [url, setUrl] = useState('')
   const uid = 'guJqAglqTLAzoMIQA6Gi'
 
   useEffect(() => {
@@ -46,22 +48,26 @@ export default function Verify() {
     }
   }, [])
 
-  /*Get path*/
   useEffect(() => {
-     console.log(router.pathname)
+    const { origin } = absoluteUrl()
+    setUrl(`${origin}${router.pathname}`)
   }, [])
+  
+  useEffect(() => {
+    console.log(url)
+  }, [url])
 
   return (
-    <div className=" px-12 md:px-32">
+    <div className="px-12 md:px-32">
       <div className="py-2">
         {discordVerified ? (
-          <p className="h-[40px] w-full md:w-fit items-center justify-center rounded-[6px] border border-[#5865f2] bg-white px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-[#5865f2]">
+          <p className="h-[40px] w-full sm:w-[160px] items-center justify-center rounded-[6px] border border-[#5865f2] bg-white px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-[#5865f2]">
             Discord Verified
           </p>
         ) : (
           <a
-            href={process.env.NEXT_PUBLIC_DISCORD_OAUTH_URL}
-            className="h-[40px] w-full md:w-fit items-center justify-center rounded-[6px] bg-[#5865f2] px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white hover:bg-[#4752c4]"
+            href={`https://discord.com/api/oauth2/authorize?client_id=997585077548617728&redirect_uri=${url}&response_type=code&scope=identify`}
+            className="h-[40px] w-full sm:w-[160px] items-center justify-center rounded-[6px] bg-[#5865f2] px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white hover:bg-[#4752c4]"
           >
             Discord
           </a>
@@ -70,14 +76,14 @@ export default function Verify() {
       <div className="py-2">
         {twitterVerified ? (
         <p
-        className="h-[40px] w-full md:w-fit items-center justify-center rounded-[6px] border border-[#1d9bf0] bg-white px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-[#1d9bf0]"
+        className="h-[40px] w-full sm:w-[160px] items-center justify-center rounded-[6px] border border-[#1d9bf0] bg-white px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-[#1d9bf0]"
       >
         Twitter Verified
       </p>
         ) : (
           <a
-          href={process.env.NEXT_PUBLIC_TWITTER_OAUTH_URL}
-          className="h-[40px] w-full md:w-fit items-center justify-center rounded-[6px] bg-[#1d9bf0] px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white hover:bg-[#1a8cd8]"
+          href={`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=MVlFOFhNVHM0UGtJYUtkbnVkMlE6MTpjaQ&redirect_uri=${url}&scope=tweet.read&state=state&code_challenge=challenge&code_challenge_method=plain`}
+          className="h-[40px] w-full sm:w-[160px] items-center justify-center rounded-[6px] bg-[#1d9bf0] px-[5px] md:px-[20px] py-[10px] text-[14px] font-semibold leading-[] text-white hover:bg-[#1a8cd8]"
         >
           Twitter
         </a>
