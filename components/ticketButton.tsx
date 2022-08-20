d121af2819d82c244bfe4dc45869cc2007141654
marrodri <laza.mar64@gmail.com>
2022-08-20 13:36:53 -0700

    eventPage refactor

--- components/ticketButton.tsx
+++ components/ticketButton.tsx
@@ -4,7 +4,7 @@ export default function TicketButton({
   ticket,
   selected
 }: {
-  ticket: TicketInterface
+  ticket: TicketInterface | null
   selected: boolean
 }) {
   return (
@@ -14,11 +14,11 @@ export default function TicketButton({
       } flex h-[76px] pt-[13px] pb-[14px] pl-[17px] w-[320px] flex-row items-center justify-between rounded-xl border bg-white px-[20px] md:w-[373px]`}
     >
       <div className="flex flex-col items-start">
-        <div className="text-[14px] font-bold">{ticket.ticketTitle}</div>
-        <div className="text-[10px]">{ticket.registeredUsers}/{ticket.capLimit} remaining</div>
-        <div className="text-[10px]"># {ticket.tokenId} required?</div>
+        <div className="text-[14px] font-bold">{ticket?.ticketTitle || 'NULL TICKET TITLE'}</div>
+        <div className="text-[10px]">{ticket?.registeredUsers || '0'}/{ticket?.capLimit || '0'} remaining</div>
+        <div className="text-[10px]"># {ticket?.tokenId || '0x000'} required?</div>
       </div>
-      <div className="text-[14px] font-bold">$ {ticket.price.toFixed(2)}</div>
+      <div className="text-[14px] font-bold">$ {ticket?.price.toFixed(2) || '0.00'}</div>
     </div>
   )
 }
--- pages/event/[eid].tsx
+++ pages/event/[eid].tsx
@@ -1,7 +1,13 @@
 // author: marthel
-import { doc, getDoc, getDocs,collection, DocumentData } from '@firebase/firestore'
+import {
+  doc,
+  getDoc,
+  getDocs,
+  collection,
+  DocumentData
+} from '@firebase/firestore'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import TicketButton from '../../components/ticketButton'
 import { db } from '../../services/firebase_config'
 import { TicketInterface } from '../../shared/interface/common'
@@ -12,23 +18,44 @@ import { useEvents } from '../../context/eventsContext'
 import Button from '../../components/button'
 import CreateCheckoutSession from './components/createCheckoutSession'
 
+/**
+ * parts that will use the loading animation
+ *  -- title
+ *  -- date and address
+ *  --google map image
+ *  -- description
+ *  -- the event image(the icon will be used for an easy fix)
+ *  -- the ticket buttons.
+ */
+
+enum EventPageEnum {
+  fetchingData,
+  fetchedData,
+  purchasedTicket
+}
+
 export default function Event() {
   // user id (this will probably be useContext)
 
-  const [fetched, setFetched] = useState(false)
-  const [event, setEvent] = useState<EventInterface>()
-  const [selectedTicket, setSelectedTicket] = useState<TicketInterface>()
-  const [selectedIndex, setSelectedIndex] = useState<number>()
+  const [eventPageStatus, setEventPageStatus] = useState<EventPageEnum>(
+    EventPageEnum.fetchingData
+  )
+  const [event, setEvent] = useState<EventInterface | null>(null)
+  const [selectedTicket, setSelectedTicket] = useState<TicketInterface | null>(
+    null
+  )
   const [showModal, setShowModal] = useState(false)
-  const [ticketPurchased, setTicketPurchased] = useState(false)
-  const [ticketListData, setTicketListData] = useState<TicketInterface[]>()
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null)
+  const [ticketListData, setTicketListData] = useState<
+    TicketInterface[] | null
+  >(null)
   const router = useRouter()
   const events = useEvents()
 
   const { eid } = router.query
 
-  const newTicketOption = (ticketDoc:any)=>{
-    const ticketData:TicketInterface={
+  const newTicketOption = (ticketDoc: any) => {
+    const ticketData: TicketInterface = {
       ticketTitle: ticketDoc.data().ticket_title,
       registeredUsers: ticketDoc.data().registered_users,
       capLimit: ticketDoc.data().cap_limit,
@@ -38,108 +65,68 @@ export default function Event() {
     return ticketData
   }
 
+  const EventPage = () => {
+    switch (eventPageStatus) {
+      case EventPageEnum.fetchedData:
+        return (
+          <LoadedEventPage event={event}>
+            <SelectAndPurchaseTicket
+              ticketListData={ticketListData}
+              selectedIndex={selectedIndex}
+              setSelectedIndex={setSelectedIndex}
+              selectedTicket={selectedTicket}
+              setSelectedTicket={setSelectedTicket}
+              setShowModal={setShowModal}
+            />
+          </LoadedEventPage>
+        )
+      case EventPageEnum.purchasedTicket:
+        return (
+          <LoadedEventPage event={event}>
+            <PurchasedTicketConfirmation selectedTicket={selectedTicket} />
+          </LoadedEventPage>
+        )
+      default:
+        return <div>Loading</div>
+    }
+  }
+
+  const confirmSelectedTicketPurchase = () => {
+    setEventPageStatus(EventPageEnum.purchasedTicket)
+  }
+
+  const handleOnClose = () => setShowModal(false)
+
   useEffect(() => {
     const fetchData = async () => {
       const eventId: any = eid
       const docRef = doc(db, 'events', eventId)
       const eventDoc = await getDoc(docRef)
       const eventData = events.newEventData(eventDoc)
-      const fetchedTicketListData : Array<TicketInterface> = []
+      const fetchedTicketListData: Array<TicketInterface> = []
       if (!eventData) return
       setEvent(eventData)
-
 
       // fetch ticket data.
       const ticketDocs = await getDocs(collection(db, 'tickets'))
 
-      ticketDocs.forEach((ticketDoc)=>{
+      ticketDocs.forEach((ticketDoc) => {
         fetchedTicketListData.push(newTicketOption(ticketDoc))
       })
       setTicketListData(fetchedTicketListData)
-      setFetched(true)
+      setEventPageStatus(EventPageEnum.fetchedData)
     }
-    if (!fetched && eid) {
+    if (eventPageStatus === EventPageEnum.fetchingData && eid) {
       fetchData()
     }
   }, [eid])
 
-  const confirmSelectedTicketPurchase = () => {
-    setTicketPurchased(true)
-  }
 
-  const handleOnClose = () => setShowModal(false)
 
   return (
     <>
       <div className="flex w-screen flex-col bg-secondaryBg px-[20px] pt-[35px] pb-[70px] sm:px-[210px] md:flex-row md:space-x-[291px] md:pb-[106px] md:pt-[85px]">
-        {fetched ? (
-          <>
-            <div className="flex h-full flex-col items-center lg:items-start ">
-              <div
-                id="event-details"
-                className="mb-[50px] w-auto  space-y-[15px] font-medium leading-[40px] md:space-y-[25px] md:text-[14px]"
-              >
-                <h3>
-                  {event?.eventTitle !== null
-                    ? event?.eventTitle
-                    : 'Event Title'}
-                </h3>
-                <div
-                  id="mobile-event-image"
-                  className="relative h-[310px] w-[310px] rounded-[67px]  px-[50px] py-[50px] lg:hidden"
-                >
-                  <Image
-                    src={event ? event.imgURL : ''}
-                    layout="fill"
-                    loading="lazy"
-                    objectFit="cover"
-                    className="rounded-[67px]"
-                  />
-                </div>
-                <div className="leading-[25px]">
-                  {event?.date} <br />
-                  {event?.address}
-                </div>
-
-                <div className="relative h-[100px] w-[100px] rounded-[20px] bg-green-100">
-                  <Image
-                    src={`https://maps.googleapis.com/maps/api/staticmap?center=City+Hall,New+York,NY&zoom=15&size=205x205&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}`}
-                    layout="fill"
-                    loading="lazy"
-                    objectFit="cover"
-                    className="rounded-[20px]"
-                  />
-                </div>
-                <div className="leading-[20px]">description of the event</div>
-              </div>
-              {!ticketPurchased ? (
-                <SelectAndPurchaseTicket
-                ticketListData={ticketListData}
-                  selectedIndex={selectedIndex}
-                  setSelectedIndex={setSelectedIndex}
-                  selectedTicket={selectedTicket}
-                  setSelectedTicket={setSelectedTicket}
-                  setShowModal={setShowModal}
-                />
-              ) : (
-                <PurchasedTicketConfirmation selectedTicket={selectedTicket} />
-              )}
-            </div>
-            <div>
-              <div className="relative hidden h-[400px] w-[400px] rounded-[67px] bg-slate-400 px-[50px] py-[50px] lg:block">
-                <Image
-                  src={event ? event.imgURL : ''}
-                  layout="fill"
-                  loading="lazy"
-                  objectFit="cover"
-                  className="rounded-[67px]"
-                />
-              </div>
-            </div>
-          </>
-        ) : (
-          <div>Loading</div>
-        )}
+        {EventPage()}
       </div>
       <Modal
         visible={showModal}
@@ -157,20 +144,82 @@ export default function Event() {
   )
 }
 
+function LoadedEventPage({
+  event,
+  children
+}: {
+  event: EventInterface | null
+  children: ReactElement
+}): JSX.Element {
+  return (
+    <>
+      <div className="flex h-full flex-col items-center lg:items-start ">
+        <div
+          id="event-details"
+          className="mb-[50px] w-auto  space-y-[15px] font-medium leading-[40px] md:space-y-[25px] md:text-[14px]"
+        >
+          <h3>
+            {event?.eventTitle !== null ? event?.eventTitle : 'Event Title'}
+          </h3>
+          <div
+            id="mobile-event-image"
+            className="relative h-[310px] w-[310px] rounded-[67px]  px-[50px] py-[50px] lg:hidden"
+          >
+            <Image
+              src={event ? event.imgURL : ''}
+              layout="fill"
+              loading="lazy"
+              objectFit="cover"
+              className="rounded-[67px]"
+            />
+          </div>
+          <div className="leading-[25px]">
+            {event?.date} <br />
+            {event?.address}
+          </div>
+
+          <div className="relative h-[100px] w-[100px] rounded-[20px] bg-green-100">
+            <Image
+              src={`https://maps.googleapis.com/maps/api/staticmap?center=City+Hall,New+York,NY&zoom=15&size=205x205&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}`}
+              layout="fill"
+              loading="lazy"
+              objectFit="cover"
+              className="rounded-[20px]"
+            />
+          </div>
+          <div className="leading-[20px]">description of the event</div>
+        </div>
+        {children}
+      </div>
+      <div>
+        <div className="relative hidden h-[400px] w-[400px] rounded-[67px] bg-slate-400 px-[50px] py-[50px] lg:block">
+          <Image
+            src={event ? event.imgURL : ''}
+            layout="fill"
+            loading="lazy"
+            objectFit="cover"
+            className="rounded-[67px]"
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
 function SelectAndPurchaseTicket({
   ticketListData,
   selectedTicket,
   setSelectedTicket,
   selectedIndex,
   setSelectedIndex,
-  setShowModal,
+  setShowModal
 }: {
-  ticketListData: Array<TicketInterface> | undefined
-  selectedIndex: number | undefined
+  ticketListData: Array<TicketInterface> | null
+  selectedIndex: number | null
   setSelectedIndex: (index: number) => void
-  selectedTicket: TicketInterface | undefined
+  selectedTicket: TicketInterface | null
   setSelectedTicket: (ticket: TicketInterface) => void
-  setShowModal: (toggle: boolean) => void,
+  setShowModal: (toggle: boolean) => void
 }) {
   return (
     <div
@@ -200,7 +249,7 @@ function SelectAndPurchaseTicket({
         onClick={() => {
           setShowModal(true)
         }}
-        active={selectedTicket !== undefined}
+        active={selectedTicket !== null}
       />
     </div>
   )
@@ -209,22 +258,11 @@ function SelectAndPurchaseTicket({
 function PurchasedTicketConfirmation({
   selectedTicket
 }: {
-  selectedTicket: TicketInterface | undefined
+  selectedTicket: TicketInterface | null
 }) {
   return (
     <div className="flex flex-col space-y-[26px]">
-      <TicketButton
-        selected={true}
-        ticket={
-          selectedTicket || {
-            ticketTitle: '',
-            registeredUsers: 0,
-            capLimit: 0,
-            tokenId: '',
-            price: 0
-          }
-        }
-      />
+      <TicketButton selected={true} ticket={selectedTicket} />
       <div className="space-y-[13px]">
         <div className="text-[14px] font-bold">
           add your ticket to your apple wallet
@@ -249,4 +287,3 @@ function PurchasedTicketConfirmation({
     </div>
   )
 }
-
