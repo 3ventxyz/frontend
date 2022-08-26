90ae5c9e62e811e9480aa1d5fe4f75c2298c0b87
marrodri <laza.mar64@gmail.com>
2022-08-25 18:23:08 -0700

    dashboard/createEvent: basic inputs and sections added

--- pages/dashboard/createEvent.tsx
+++ pages/dashboard/createEvent.tsx
@@ -1,8 +1,59 @@
+import TextInput from '../../components/textInput'
 
-
-
-
-
-export default function CreateEvent(){
-	return <div>TODO: create Event</div>
+export default function CreateEvent() {
+  return (
+    <div className="flex w-screen flex-col items-center bg-secondaryBg pt-[35px] pb-[35px]">
+      TODO: create Event
+      <div>
+        <h4>Event Name:</h4>
+        <TextInput
+          id={''}
+          labelText={''}
+          setValue={(e: any) => {
+            console.log('event name: ' + e)
+          }}
+        />
+      </div>
+      <div>
+        <h4>Organization:</h4>
+        <TextInput
+          id={''}
+          labelText={''}
+          setValue={(e: any) => {
+            console.log('event name: ' + e)
+          }}
+        />
+      </div>
+      <div>
+        <h4>Event Tile Picture:</h4>
+		<TextInput
+          id={''}
+          labelText={''}
+          setValue={(e: any) => {
+            console.log('event name: ' + e)
+          }}
+        />
+      </div>
+      <div>
+        <h4>Address and date of event</h4>
+		<TextInput
+          id={''}
+          labelText={''}
+          setValue={(e: any) => {
+            console.log('event name: ' + e)
+          }}
+        />
+      </div>
+      <div>
+        <h4>Ticket tiles</h4>
+		<TextInput
+          id={''}
+          labelText={''}
+          setValue={(e: any) => {
+            console.log('event name: ' + e)
+          }}
+        />
+      </div>
+    </div>
+  )
 }
