b95ec18e2fce87704a9ce03a99cbe5ebd49f6657
marrodri <laza.mar64@gmail.com>
2022-08-27 19:47:47 -0700

    dashboard/createEvent: layout padding update; add event picture section added; create new event button added

--- pages/dashboard/createEvent.tsx
+++ pages/dashboard/createEvent.tsx
@@ -1,10 +1,12 @@
+import { TbPhoto } from 'react-icons/tb'
 import TextInput from '../../components/textInput'
-
+import { MdOutlineAddPhotoAlternate } from 'react-icons/md'
+import Button from '../../components/button'
 export default function CreateEvent() {
   return (
-    <div className="flex w-screen flex-col items-center bg-secondaryBg pt-[35px] pb-[35px]">
-      TODO: create Event
-      <div>
+    <div className="flex w-screen flex-col items-start space-y-[15px] bg-secondaryBg pb-[100px] pt-[35px] pl-[150px]">
+      <h1>Create new event</h1>
+      <div className="">
         <h4>Event Name:</h4>
         <TextInput
           id={''}
@@ -26,17 +28,15 @@ export default function CreateEvent() {
       </div>
       <div>
         <h4>Event Tile Picture:</h4>
-		<TextInput
-          id={''}
-          labelText={''}
-          setValue={(e: any) => {
-            console.log('event name: ' + e)
-          }}
-        />
+        <div className="relative h-[384px] max-h-[320px] w-[380px] rounded-3xl bg-gray-300 sm:max-h-full">
+          <div className="flex h-full w-full flex-col items-center justify-center text-gray-400">
+            <MdOutlineAddPhotoAlternate className="h-[150px] w-[150px]" />
+          </div>
+        </div>
       </div>
       <div>
         <h4>Address and date of event</h4>
-		<TextInput
+        <TextInput
           id={''}
           labelText={''}
           setValue={(e: any) => {
@@ -46,7 +46,7 @@ export default function CreateEvent() {
       </div>
       <div>
         <h4>Ticket tiles</h4>
-		<TextInput
+        <TextInput
           id={''}
           labelText={''}
           setValue={(e: any) => {
@@ -54,6 +54,7 @@ export default function CreateEvent() {
           }}
         />
       </div>
+      <Button type='submit' text={'Create new event'} onClick={()=>{console.log('new event added')}} active={true}/>
     </div>
   )
 }
