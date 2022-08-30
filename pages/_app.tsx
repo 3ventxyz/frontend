ce3da479fe608c5f43f91474eee83133edeac927
Locke <benkim96@gmail.com>
2022-08-30 13:28:18 -0700

    move script component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,11 +40,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.svg" />
-        <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`}
-          async
-        ></Script>
       </Head>
+      <Script
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`}
+        async
+      ></Script>
       <AuthProvider>
         <EventsProvider>
           <WagmiConfig client={wagmiClient}>
