6fd45f842d8570aedf18574ae160756630d24e99
marrodri <laza.mar64@gmail.com>
2022-08-30 18:53:02 -0700

    components/fileImageInput: hover functionality added; when hovering over the component, after selecting an image

--- components/fileImageInput.tsx
+++ components/fileImageInput.tsx
@@ -3,27 +3,43 @@ import { useState } from 'react'
 import { MdOutlineAddPhotoAlternate } from 'react-icons/md'
 import { storage } from '../services/firebase_config'
 import Image from 'next/image'
-// TODO add the logic of adding a picture from the desktop computer
+
 export default function FileImageInput() {
-  // const [fileImg, setFileImg] = useState<File | null>(null)
   const [fileImg, setFileImg] = useState<File | null>(null)
   const [imgURl, setImgUrl] = useState('')
-
-  const fileImgHoverContent = () => {}
+  const [isMouseHover, setMouseHover] = useState<boolean>(false)
+  const [fileErrorMsg, setFileErrorMsg] = useState<String | null>(null)
 
   return fileImg !== null ? (
-    <div className="relative h-[384px] max-h-[320px] w-[380px] rounded-3xl bg-gray-300 hover:cursor-pointer hover:bg-gray-500 sm:max-h-full">
-      <Image src={imgURl}
-      layout="fill"
-      objectFit='cover'
+    <div
+      onMouseEnter={() => {
+        console.log('mouse hovered on image')
+        setMouseHover(true)
+      }}
+      onMouseLeave={() => {
+        console.log('mouse stop hovering on image')
+        setMouseHover(false)
+      }}
+      className="relative h-[384px] max-h-[320px] w-[380px] rounded-3xl bg-gray-300 hover:cursor-pointer hover:bg-gray-500 sm:max-h-full"
+    >
+      <Image
+        src={imgURl}
+        layout="fill"
+        objectFit="cover"
+        className="rounded-3xl"
       />
+      {isMouseHover ? (
+        <div className="flex h-full w-full items-center justify-center rounded-3xl bg-gray-400 text-white opacity-70">
+          Please click to change your image
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   ) : (
     <div className="relative h-[384px] max-h-[320px] w-[380px] rounded-3xl bg-gray-300 hover:cursor-pointer sm:max-h-full">
       <input
         type="file"
-        // find the type for the single file; do NOT use files.!!!!
-        // the type is FileList
         onChange={(event: any) => {
           console.log(
             'uploading picture, no big than 600px and no big than 10 mbs'
@@ -31,8 +47,7 @@ export default function FileImageInput() {
           setFileImg(event.target.files[0])
           console.log('=========================================')
           console.log(event.target.files)
-          
-          
+
           console.log('=========================================')
           console.log('+++++URL+++++++++++++++++++++++++++++++++++')
           console.log(URL.createObjectURL(event.target.files[0]))
