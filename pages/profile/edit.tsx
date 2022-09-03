a850472a7afbb540ee40009aa125678e2438a84f
constanzadl <constanzadl94@gmail.com>
2022-09-03 14:38:43 -0500

    edit page

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,20 +1,26 @@
 import TextInput from '../../components/textInput'
- import LocationInput from '../../components/locationInput'
- import { useState } from 'react'
- import FileInput from '../../components/fileInput'
- import Button from '../../components/button'
- import { db } from '../../services/firebase_config'
- import { doc, updateDoc } from 'firebase/firestore'
- import Link from 'next/link'
- import { useAuth } from '../../contexts/auth'
+import LocationInput from '../../components/locationInput'
+import { useState, useEffect } from 'react'
+import FileInput from '../../components/fileInput'
+import Button from '../../components/button'
+import { db } from '../../services/firebase_config'
+import { doc, updateDoc, getDoc } from 'firebase/firestore'
+import Link from 'next/link'
+import { useAuth } from '../../contexts/auth'
 
- interface LocationData {
-   lat: number
-   long: number
-   address: string
- }
+interface LocationData {
+  lat: number
+  long: number
+  address: string
+}
 
- const CreateProfile = async (uid: string, name: string, bio: string, address = '', gravatarLink= '') => {
+const CreateProfile = async (
+  uid: string,
+  name: string,
+  bio: string,
+  address = '',
+  gravatarLink = ''
+) => {
   try {
     const docRef = doc(db, 'users', uid)
     await updateDoc(docRef, {
@@ -28,76 +34,123 @@ import TextInput from '../../components/textInput'
   } catch (e) {
     console.error('Error adding data: ', e)
   }
- }
+}
 
- const fetchAvatar = (email: string, setGravatar: any) => {
-  const md5 = require( 'md5' );
+const fetchAvatar = (email: string, setGravatar: any) => {
+  const md5 = require('md5')
 
-  function getGravatarURL( email: string ) {
-    const address = String( email ).trim().toLowerCase();
-    const hash = md5( address );
-    const url = `https://www.gravatar.com/avatar/${ hash }`;
+  function getGravatarURL(email: string) {
+    const address = String(email).trim().toLowerCase()
+    const hash = md5(address)
+    const url = `https://www.gravatar.com/avatar/${hash}`
     console.log('url', url)
-    return url;
+    return url
   }
   const url = getGravatarURL(email)
   setGravatar(url)
- }
- export default function CreateOrganization() {
-   const [name, setName] = useState('')
-   const [bio, setBio] = useState('')
-   const [location, setLocation] = useState<LocationData>()
-   const [email, setEmail] = useState('')
-   const [gravatar, setGravatar] = useState('')
-   const auth = useAuth()
-   const uid = auth?.uid
-   
-   return (
-     <div className="flex w-full flex-col items-center space-y-4 px-4 py-[43px] text-center sm:px-0 bg-secondaryBg">
-        <div className="w-1/2 text-left">
-        <h3 className="pb-[40px]">Create your Profile</h3>
-       <TextInput
-         labelText="Username"
-         id="username"
-         placeholder="Username"
-         maxWidth={500}
-         width={'w-auto'}
-         textArea={false}
-         setValue={setName}
-       />
-       <TextInput
-         labelText="Bio"
-         id="bio"
-         placeholder="Bio"
-         maxWidth={500}
-         width={'w-auto'}
-         textArea={false}
-         setValue={setBio}
-       />
-       <LocationInput
-         labelText="Location"
-         id="user_location"
-         placeholder="Where are you based?"
-         setLocation={setLocation}
-       />
-   <p className="text-left text-[16px] font-semibold py-4">Avatar</p>
-   <FileInput />
-   <TextInput
-          labelText="Email"
-          id="email"
-          placeholder="Email"
-          maxWidth={500}
-          width={'w-auto'}
-          textArea={false}
-          setValue={setEmail}
-        />
-    <button onClick={() => {fetchAvatar(email, setGravatar)}}>Set with Gravatar</button>
-   <Link href="/profile">      
-      <Button text="Save" onClick={() => {
-        CreateProfile(uid, name, bio, location?.address, gravatar)
-   }} active={true}/>
-   </Link>
-     </div>
+}
+
+export default function CreateUser() {
+  const [name, setName] = useState('')
+  const [bio, setBio] = useState('')
+  const [location, setLocation] = useState<LocationData>()
+  const [email, setEmail] = useState('')
+  const [avatar, setAvatar] = useState('')
+  const auth = useAuth()
+  const uid = auth?.uid
+  let remakeProfile = false
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const docRef = doc(db, 'users', uid)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        setName(docSnap.data().username)
+        setBio(docSnap.data().bio)
+        setLocation(docSnap.data().location)
+        setAvatar(`${docSnap.data().gravatar}?s=200`)
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such document!')
+      }
+    }
+    getInfo()
+  }, [remakeProfile])
+
+  return (
+    <div className="w-full items-center space-y-4 bg-secondaryBg px-4 py-[40px] text-center sm:px-0">
+      <div className="mx-auto w-1/2 content-center"></div>
+      <div className="mx-auto flex w-1/2 content-center justify-around">
+        <div className="w-1/2 p-2 text-left">
+          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+            Username
+          </p>
+          <TextInput
+            labelText=""
+            id="username"
+            placeholder={name}
+            maxWidth={500}
+            width={'w-auto'}
+            textArea={false}
+            setValue={setName}
+          />
+          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+            Bio
+          </p>
+          <TextInput
+            labelText="Bio"
+            id="bio"
+            placeholder={bio}
+            maxWidth={500}
+            width={'w-auto'}
+            textArea={false}
+            setValue={setBio}
+          />
+          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+            Location
+          </p>
+          <LocationInput
+            labelText="Location"
+            id="user_location"
+            placeholder="Where are you located?"
+            setLocation={setLocation}
+          />
         </div>
-   )
- }
+        <div>
+          <img src={avatar} />
+          <TextInput
+            labelText="Email"
+            id="email"
+            placeholder="Enter gravatar email"
+            maxWidth={500}
+            width={'w-auto'}
+            textArea={false}
+            setValue={setEmail}
+          />
+        </div>
+      </div>
+      <div>
+        <Link href="/profile">
+          <Button
+            text="Save"
+            onClick={() => {
+              CreateProfile(uid, name, bio, location?.address, avatar)
+            }}
+            active={true}
+          />
+        </Link>
+        <Link href="/profile">
+          <Button
+            text="Discard"
+            onClick={() => {
+              remakeProfile = true
+            }}
+            active={true}
+          />
+        </Link>
+      </div>
+      <p>Events</p>
+    </div>
+  )
+}
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,56 +1,64 @@
 import TextInput from '../../components/textInput'
- import LocationInput from '../../components/locationInput'
- import { useEffect, useState } from 'react'
- import FileInput from '../../components/fileInput'
- import Button from '../../components/button'
- import { db } from '../../services/firebase_config'
- import { doc, getDoc } from "firebase/firestore";
- import { useAuth } from '../../contexts/auth'
- import Link from 'next/link'
+import LocationInput from '../../components/locationInput'
+import { useEffect, useState } from 'react'
+import FileInput from '../../components/fileInput'
+import Button from '../../components/button'
+import { db } from '../../services/firebase_config'
+import { doc, getDoc } from 'firebase/firestore'
+import { useAuth } from '../../contexts/auth'
+import Link from 'next/link'
 
- export default function UserProfile() {
-   const [name, setName] = useState('')
-   const [bio, setBio] = useState('')
-   const [location, setLocation] = useState('')
-   const [avatar, setAvatar] = useState('')
-   const auth = useAuth()
-   const uid = auth?.uid
+export default function UserProfile() {
+  const [name, setName] = useState('')
+  const [bio, setBio] = useState('')
+  const [location, setLocation] = useState('')
+  const [avatar, setAvatar] = useState('')
+  const auth = useAuth()
+  const uid = auth?.uid
 
-   useEffect(() => {
+  useEffect(() => {
     const getInfo = async () => {
-        const docRef = doc(db, "users", uid);
-        const docSnap = await getDoc(docRef);
-        
-        if (docSnap.exists()) {
-          setName(docSnap.data().username)
-          setBio(docSnap.data().bio)
-          setLocation(docSnap.data().location)
-          setAvatar(`${docSnap.data().gravatar}?s=200`)
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
+      const docRef = doc(db, 'users', uid)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        setName(docSnap.data().username)
+        setBio(docSnap.data().bio)
+        setLocation(docSnap.data().location)
+        setAvatar(`${docSnap.data().gravatar}?s=200`)
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such document!')
+      }
     }
     getInfo()
-    },[])
+  }, [])
 
-   return (
-     <div className="w-full items-center space-y-4 px-4 text-center sm:px-0 bg-secondaryBg py-[40px]">
-      <div className="w-1/2 mx-auto content-center">
-         <Link href="/profile/edit" className="float-right"><p className="cursor-pointer hover:underline text-right">edit</p></Link>
+  return (
+    <div className="w-full items-center space-y-4 bg-secondaryBg px-4 py-[40px] text-center sm:px-0">
+      <div className="mx-auto w-1/2 content-center">
+        <Link href="/profile/edit" className="float-right">
+          <p className="cursor-pointer text-right hover:underline">edit</p>
+        </Link>
       </div>
-         <div className="flex justify-around w-1/2 mx-auto content-center">
-            <div className="text-left w-1/2 p-2">
-                <p className="text-left text-[16px] font-semibold pt-2 mb-2 border-b border-primary">Username</p>
-                <p className="p-1 text-secondary">{name}</p>
-                <p className="text-left text-[16px] font-semibold pt-2 mb-2 border-b border-primary">Bio</p>       
-                <p className="p-1 text-secondary">{bio}</p>
-                <p className="text-left text-[16px] font-semibold pt-2 mb-2 border-b border-primary">Location</p>
-                <p className="p-1 text-secondary">{location}</p>
-            </div>
-            <img src={avatar} />
+      <div className="mx-auto flex w-1/2 content-center justify-around">
+        <div className="w-1/2 p-2 text-left">
+          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+            Username
+          </p>
+          <p className="p-1 text-secondary">{name}</p>
+          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+            Bio
+          </p>
+          <p className="p-1 text-secondary">{bio}</p>
+          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+            Location
+          </p>
+          <p className="p-1 text-secondary">{location}</p>
         </div>
-        <p>Events</p>
+        <img src={avatar} />
+      </div>
+      <p>Events</p>
     </div>
-   )
- }
+  )
+}
