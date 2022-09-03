8e8984c7cfe770ba9820153a680ae329e50691ad
Locke <benkim96@gmail.com>
2022-09-03 12:31:17 -0700

    remove unnecessary script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,10 +44,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.svg" />
-        <script
-          src={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&libraries=places`}
-          async
-        ></script>
       </Head>
       <Script
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`}
