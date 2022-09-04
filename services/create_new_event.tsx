3c97994f33e76bed8961c73dfbf6cc1fcadc034c
marrodri <46203279+marrodri@users.noreply.github.com>
2022-09-04 15:05:58 -0700

    Marthel/event creation 1.1 (#30)
    * verification parameterized url; vercel dev domain deploy
    
    * events dashboard firebase fix
    
    * event/createEvent: basic dateTime input picker added
    
    * event/createEvent: start_date and end_date dateTime pickers added
    
    Co-authored-by: Locke <locke@Lockes-MacBook-Pro.local>

--- services/create_new_event.tsx
+++ services/create_new_event.tsx
@@ -6,11 +6,15 @@ export async function createNewEvent(
   newEventData: NewEventInterface | null,
   fileImg: File | null
 ) {
+  console.log('newEventData.startDate: ' + newEventData?.start_date)
+  console.log('newEventData.endDate: ' + newEventData?.end_date)
   try {
     if (!newEventData) {
       throw 'error event data is empty'
     }
     const docRef = await addDoc(collection(db, 'events'), newEventData)
+    console.log('docRef id: ' + docRef.id)
+    console.log('===================')
   } catch (err) {
     console.log(err)
     return false
