bdbf16fdb561553767581b636ae60d1c291050ea
Locke <benkim96@gmail.com>
2022-09-04 19:03:52 -0700

    refactor profile components

--- pages/u/[uid].tsx
+++ pages/u/[uid].tsx
@@ -1,15 +1,10 @@
 import { useEffect, useState } from 'react'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../../services/firebase_config'
 import Dashboard from '../../components/dashboard'
-
-interface LocationData {
-  lat: number
-  long: number
-  address: string
-}
+import ProfileDisplay from './components/profileDisplay'
+import { LocationData } from '../../shared/interface/common'
 
 export default function UserProfile() {
   const [name, setName] = useState('')
@@ -43,47 +38,13 @@ export default function UserProfile() {
 
   return (
     <div className="flex w-full flex-col items-start space-y-4 bg-secondaryBg px-4 py-[40px] text-center sm:px-0">
-      <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-8 xl:justify-start">
-        <div className="relative h-[384px] max-h-[320px] w-full max-w-[380px] rounded-3xl bg-gray-200 sm:max-h-full">
-          <Image
-            src={avatar}
-            layout="fill"
-            loading="lazy"
-            objectFit="cover"
-            className="rounded-3xl"
-          />
-        </div>
-        <div className="flex w-full max-w-[380px] flex-col items-stretch justify-start text-left">
-          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
-            Username
-          </p>
-          <p className="p-1 text-secondary">{name}</p>
-          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
-            Bio
-          </p>
-          <p className="p-1 text-secondary">{bio}</p>
-          <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
-            Location
-          </p>
-          <p className="p-1 text-secondary">{location?.address}</p>
-          {twitterName && (
-            <>
-              <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
-                Twitter
-              </p>
-              <a
-                className="p-1 text-linkText"
-                href={`https://twitter.com/${twitterName}`}
-                rel="noreferrer"
-                target="_blank"
-              >
-                Twitter
-              </a>
-            </>
-          )}
-          <div className="h-4" />
-        </div>
-      </div>
+      <ProfileDisplay
+        name={name}
+        bio={bio}
+        img={avatar}
+        address={location?.address || 'NA'}
+        twitterName={twitterName}
+      />
       <Dashboard />
     </div>
   )
---
+++ pages/u/components/profileDisplay.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image'
+
+function TextDisplay({ label, value }: { label: string; value: string }) {
+  return (
+    <>
+      <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+        {label}
+      </p>
+      <p className="p-1 text-secondary">{value}</p>
+    </>
+  )
+}
+
+export default function ProfileDisplay({
+  name,
+  bio,
+  address,
+  twitterName,
+  img
+}: {
+  name: string
+  bio: string
+  address: string
+  twitterName: string
+  img: string
+}) {
+  return (
+    <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-8 xl:justify-start">
+      <div className="relative h-[384px] max-h-[320px] w-full max-w-[380px] rounded-3xl bg-gray-200 sm:max-h-full">
+        <Image
+          src={img}
+          layout="fill"
+          loading="lazy"
+          objectFit="cover"
+          className="rounded-3xl"
+        />
+      </div>
+      <div className="flex w-full max-w-[380px] flex-col items-stretch justify-start text-left">
+        <TextDisplay label="Username" value={name} />
+        <TextDisplay label="Bio" value={bio} />
+        <TextDisplay label="Location" value={address || 'NA'} />
+        {twitterName && (
+          <>
+            <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+              Twitter
+            </p>
+            <a
+              className="p-1 text-linkText"
+              href={`https://twitter.com/${twitterName}`}
+              rel="noreferrer"
+              target="_blank"
+            >
+              Twitter
+            </a>
+          </>
+        )}
+        <div className="h-4" />
+      </div>
+    </div>
+  )
+}
