d1844836e8472d693e26979deec1f312b80be480
marrodri <laza.mar64@gmail.com>
2022-09-09 15:47:05 -0700

    user/edit: basi modal component added for displaying the qr code

--- pages/u/components/profileDisplay.tsx
+++ pages/u/components/profileDisplay.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 import Button from '../../../components/button'
+import Modal from '../../../components/modal'
+import QRCodeGenerator from './qrCodeGenerator'
 
 function TextDisplay({ label, value }: { label: string; value: string }) {
   return (
@@ -19,6 +22,7 @@ export default function ProfileDisplay({
   address,
   twitterName,
   img,
+  uid = '',
   edit = false
 }: {
   name: string
@@ -26,51 +30,80 @@ export default function ProfileDisplay({
   address: string
   twitterName: string
   img: string
+  uid?: string
   edit?: boolean
 }) {
+  const [showModal, setShowModal] = useState(false)
+
+  const onCloseModal = () => {
+    setShowModal(false)
+  }
   return (
-    <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-8 xl:justify-start">
-      <div className="relative h-[384px] max-h-[320px] w-full max-w-[380px] rounded-3xl bg-gray-200 sm:max-h-full">
-        <Image
-          src={img}
-          layout="fill"
-          loading="lazy"
-          objectFit="cover"
-          className="rounded-3xl"
-        />
+    <>
+      <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-8 xl:justify-start">
+        <div className="relative h-[384px] max-h-[320px] w-full max-w-[380px] rounded-3xl bg-gray-200 sm:max-h-full">
+          <Image
+            src={img}
+            layout="fill"
+            loading="lazy"
+            objectFit="cover"
+            className="rounded-3xl"
+          />
+        </div>
+        <div className="flex w-full max-w-[380px] flex-col items-stretch justify-start text-left">
+          <TextDisplay label="Username" value={name} />
+          <TextDisplay label="Bio" value={bio} />
+          <TextDisplay label="Location" value={address || 'NA'} />
+          {twitterName && (
+            <>
+              <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
+                Twitter
+              </p>
+              <a
+                className="p-1 text-linkText"
+                href={`https://twitter.com/${twitterName}`}
+                rel="noreferrer"
+                target="_blank"
+              >
+                Twitter
+              </a>
+            </>
+          )}
+          <div className="h-4" />
+          {edit && (
+            <div className="flex flex-row space-x-[10px]">
+              <div>
+                <Link href="/u/edit">
+                  <Button
+                    text={'Edit Profile'}
+                    onClick={() => {
+                      return
+                    }}
+                    active={true}
+                  />
+                </Link>
+              </div>
+              <div>
+                <Button
+                  text={'Show QR code'}
+                  onClick={() => {
+                    setShowModal(true)
+                  }}
+                  active={true}
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="flex w-full max-w-[380px] flex-col items-stretch justify-start text-left">
-        <TextDisplay label="Username" value={name} />
-        <TextDisplay label="Bio" value={bio} />
-        <TextDisplay label="Location" value={address || 'NA'} />
-        {twitterName && (
-          <>
-            <p className="mb-2 border-b border-primary pt-2 text-left text-[16px] font-semibold">
-              Twitter
-            </p>
-            <a
-              className="p-1 text-linkText"
-              href={`https://twitter.com/${twitterName}`}
-              rel="noreferrer"
-              target="_blank"
-            >
-              Twitter
-            </a>
-          </>
-        )}
-        <div className="h-4" />
-        {edit && (
-          <Link href="/u/edit">
-            <Button
-              text={'Edit Profile'}
-              onClick={() => {
-                return
-              }}
-              active={true}
-            />
-          </Link>
-        )}
-      </div>
-    </div>
+      <Modal
+        visible={showModal}
+        onClose={onCloseModal}
+        width={'w-[500px]'}
+        height={'h-[500px]'}
+      >
+        <QRCodeGenerator uid={uid} />
+      </Modal>
+    </>
   )
 }
---
+++ pages/u/components/qrCodeGenerator.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function QRCodeGenerator({uid}:{uid:string}){
+	return <div>
+
+		QR code generated with the uid of 
+		{uid}
+	</div>
+}
+
