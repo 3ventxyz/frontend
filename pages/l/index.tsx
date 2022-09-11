b3790b9fd34da0fdd546dd08a17edc6688776bef
Aline Ayumi <aline.ayumi@unico.io>
2022-09-11 03:17:19 -0300

    feat: adding confirmation delete modal

--- pages/l/index.tsx
+++ pages/l/index.tsx
@@ -14,6 +14,8 @@ export default function Allowlists() {
   const auth = useAuth()
   const router = useRouter()
   const [showModal, setShowModal] = useState(false)
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [currentAllowlist, setCurrentAllowlist] = useState<string | undefined>()
 
   useEffect(() => {
     getAllowlists()
@@ -92,7 +94,10 @@ export default function Allowlists() {
                         width="20"
                       />
                       <Image
-                        onClick={() => deleteAllowlist(e.allowlist_id)}
+                        onClick={() => {
+                          setCurrentAllowlist(e?.allowlist_id)
+                          setShowDeleteModal(true)
+                        }}
                         alt="delete"
                         src="/assets/trash.svg"
                         height="20"
@@ -114,10 +119,76 @@ export default function Allowlists() {
       >
         <CreateAllowlistForm
           onSuccess={() => {
-            window.location.reload()
+            getAllowlists()
             setShowModal(false)
           }}
         />
+      </Modal>
+      <Modal
+        visible={showDeleteModal}
+        onClose={() => setShowDeleteModal(false)}
+        width=""
+        height=""
+      >
+        <div className="relative h-full w-full max-w-md p-4 md:h-auto">
+          <button
+            type="button"
+            className="absolute top-3 right-2.5 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
+            onClick={() => setShowDeleteModal(false)}
+          >
+            <svg
+              aria-hidden="true"
+              className="h-5 w-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+          <div className="p-6 text-center">
+            <svg
+              aria-hidden="true"
+              className="mx-auto mb-4 h-14 w-14 text-gray-400 "
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <h3 className="mb-5 text-lg font-normal text-gray-500 ">
+              Are you sure you want to delete this Allowlist?
+            </h3>
+            <button
+              onClick={() => {
+                deleteAllowlist(currentAllowlist)
+                setShowDeleteModal(false)
+              }}
+              type="button"
+              className="mr-2 inline-flex items-center rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 "
+            >
+              Yes, I&apos;m sure
+            </button>
+            <button
+              onClick={() => setShowDeleteModal(false)}
+              type="button"
+              className="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 "
+            >
+              No, cancel
+            </button>
+          </div>
+        </div>
       </Modal>
     </>
   )
