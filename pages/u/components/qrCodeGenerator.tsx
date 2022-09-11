8a50c4a8adb6928935987b898beed539c58f9929
marrodri <laza.mar64@gmail.com>
2022-09-10 21:56:34 -0700

    user/edit: qr code generated in the frontend, is now being uploaded as an image yo the firebase storage, directly to the user directory path;

--- pages/u/components/qrCodeGenerator.tsx
+++ pages/u/components/qrCodeGenerator.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState, useRef } from 'react'
 import QRCode from 'qrcode.react'
-
+import Button from '../../../components/button'
+import { uploadQRImage } from '../../../services/upload_qr_image'
+import { useAuth } from '../../../contexts/auth'
 export default function QRCodeGenerator({ uid }: { uid: string }) {
+  const qrRef = useRef<any>()
+  const auth = useAuth()
+  const uploadQRCode = (evt: FormEvent) => {
+    evt.preventDefault()
+    let canvas = qrRef.current.querySelector('canvas')
+    uploadQRImage(canvas, `${auth.uid}/qrCode`, async (url) => {
+      console.log('SUCCESS URL:', url)
+    })
+  }
+
   const qrCode = (
     <QRCode
       id="qrCodeId"
       size={300}
       value={`https://www.3vent.xyz/u/${uid}`}
       bgColor="white"
+      includeMargin={true}
       fgColor="black"
       level="H"
       imageSettings={{
@@ -23,11 +36,26 @@ export default function QRCodeGenerator({ uid }: { uid: string }) {
       <div className="grow"></div>
       <h3>This is your QR code.</h3>
       <div className="grow"></div>
-      <div className="mb-[10px] flex h-[315px] w-[315px] items-center justify-center rounded-xl bg-gray-200 shadow-xl">
-        {qrCode}
+      <div className="mb-[10px] flex h-[315px] w-[315px] items-center justify-center rounded-xl bg-gray-100 shadow-xl">
+        <div className="qr-container__qr-code" ref={qrRef}>
+          {qrCode}
+        </div>
       </div>
       <p className="text-red-400">Please DO NOT share this to the public.</p>
       <div className="grow"></div>
+
+      <div>
+        <form action="" onSubmit={uploadQRCode}>
+          <Button
+            text={'Download qr code'}
+            type={'submit'}
+            active={true}
+            onClick={() => {
+              console.log('download')
+            }}
+          />
+        </form>
+      </div>
     </div>
   )
 }
