cbfc9b1841742c849bd9de93e53fa4b76b15e53a
marrodri <laza.mar64@gmail.com>
2022-09-11 16:25:36 -0700

    user/edit: loading spinner added to the qr code generator

--- pages/u/components/qrCodeGenerator.tsx
+++ pages/u/components/qrCodeGenerator.tsx
@@ -1,16 +1,20 @@
-import React, { FormEvent, useState, useRef } from 'react'
+import React, { FormEvent, useState, useRef, useEffect } from 'react'
 import QRCode from 'qrcode.react'
 import Button from '../../../components/button'
 import { uploadQRImage } from '../../../services/upload_qr_image'
 import { useAuth } from '../../../contexts/auth'
 import { db } from '../../../services/firebase_config'
 import { doc, updateDoc } from '@firebase/firestore'
+import Spinner from '../../../components/spinner'
 export default function QRCodeGenerator({ uid }: { uid: string }) {
   const qrRef = useRef<any>()
   const auth = useAuth()
+  const [qrCodeImg, setQrCodeImg] = useState()
+  const [isFetched, setIsFetched] = useState<boolean>(true)
   const uploadQRCode = (evt: FormEvent) => {
     evt.preventDefault()
     let canvas = qrRef.current.querySelector('canvas')
+
     uploadQRImage(canvas, `${auth.uid}/qrCode`, async (url) => {
       try {
         const userDocRef = await doc(db, 'users', auth.uid)
@@ -21,6 +25,25 @@ export default function QRCodeGenerator({ uid }: { uid: string }) {
       console.log('SUCCESS URL:', url)
     })
   }
+
+  useEffect(() => {
+    /**
+    //retrieve the qr code from the firestore db,
+    // if the qrcode image is null, meaning that the field doesn't exist, then 
+     * show a message telling the user that his qr code is not created yet, click the button "generate qr code " to create
+     * and upload a new qr code, and theen show the component with the qr as shown below.
+     * 
+     * Otherwise, if a link to a qrCodeImg already exists in the firestore db and its successfully retrieved, then use that one. 
+     */
+    //
+
+    const fetchQrCode = async () => {
+      // setIsFetched(true)
+    }
+    if (!isFetched) {
+      fetchQrCode()
+    }
+  }, [])
 
   const qrCode = (
     <QRCode
@@ -39,7 +62,7 @@ export default function QRCodeGenerator({ uid }: { uid: string }) {
       }}
     />
   )
-  return (
+  return isFetched ? (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <div className="grow"></div>
       <h3>This is your QR code.</h3>
@@ -64,6 +87,10 @@ export default function QRCodeGenerator({ uid }: { uid: string }) {
           />
         </form>
       </div>
+    </div>
+  ) : (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <Spinner />
     </div>
   )
 }
