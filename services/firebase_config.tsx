3cefa2df767fc45d8f37f8626c2a31b777ca2843
marrodri <laza.mar64@gmail.com>
2022-09-30 17:36:47 -0700

    firebase emulators install: auth, cloud functions, storage, firestore

--- services/firebase_config.tsx
+++ services/firebase_config.tsx
@@ -1,8 +1,9 @@
 import { initializeApp } from '@firebase/app'
 import { getAnalytics, isSupported } from '@firebase/analytics'
-import { getFirestore } from '@firebase/firestore'
-import { getAuth } from '@firebase/auth'
-import { getStorage } from '@firebase/storage'
+import { getFirestore, connectFirestoreEmulator } from '@firebase/firestore'
+import { getAuth, connectAuthEmulator } from '@firebase/auth'
+import { getStorage, connectStorageEmulator } from '@firebase/storage'
+import { connectFunctionsEmulator, getFunctions } from '@firebase/functions'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,7 +19,15 @@ const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
 const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null))
+const functions = getFunctions(app)
 const storage = getStorage(app)
 
 export { analytics, app, auth, db, storage }
 
+if (typeof window !== 'undefined') {
+  if (window.location.hostname.includes('localhost')) {
+    connectFunctionsEmulator(functions, 'localhost', 5001)
+    connectAuthEmulator(auth, 'http://localhost:9099')
+    connectFirestoreEmulator(db, 'localhost', 8080)
+  }
+}
