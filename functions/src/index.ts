561ea7702a1d7ac5afd2b72fa5332d16ab914363
marrodri <laza.mar64@gmail.com>
2022-10-03 18:11:42 -0700

    firebase functions: package.json.lock file deleted

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,11 @@ import * as functions from "firebase-functions";
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from 2Firebase!");
+});
+
+
+export const helloWorld2 = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs2!", {structuredData: true});
+  response.send("Hello from 3Firebase at the second function!");
 });
