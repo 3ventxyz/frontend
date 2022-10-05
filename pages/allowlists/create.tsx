b60e59266f06e14d813ee82bc3f663a979c286ad
Locke <benkim96@gmail.com>
2022-10-05 15:58:11 -0700

    refactor create page for allowlistservice

--- pages/allowlists/create.tsx
+++ pages/allowlists/create.tsx
@@ -1,76 +1,11 @@
 // author: Ben
-import TextInput from '../../components/textInput'
-import Button from '../../components/button'
 import { useRouter } from 'next/router'
 import { useAuth } from '../../contexts/auth'
 
 import React, { FormEvent, useState } from 'react'
+import AllowlistService from '../../services/allowlists'
+import Button from '../../components/button'
 import ErrorAlert from '../../components/alerts/errorAlert'
-import AllowlistService from '../../../services/allowlists'
-
-// return (
-//   <form className="m-4" onSubmit={handleSubmit}>
-//     <div className="mb-6">
-//       <label
-//         htmlFor="title"
-//         className="mb-2 block text-sm font-medium text-gray-900 "
-//       >
-//         TITLE
-//       </label>
-//       <input
-//         type="text"
-//         id="title"
-//         ref={titleRef}
-//         className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-//         required
-//       />
-//     </div>
-//     <div className="mb-6">
-//       <label
-//         htmlFor="description"
-//         className="mb-2 block text-sm font-medium text-gray-900 "
-//       >
-//         DESCRIPTION
-//       </label>
-//       <input
-//         type="text"
-//         id="description"
-//         ref={descriptionRef}
-//         className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-//       />
-//     </div>
-//     <div className="mb-6">
-//       <label
-//         htmlFor="message"
-//         className="mb-2 block text-sm font-medium text-gray-900"
-//       >
-//         ADDRESSES
-//       </label>
-//       <textarea
-//         id="message"
-//         rows={4}
-//         ref={allowlistRef}
-//         className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-//         placeholder="Add the list of addresses separated with comma"
-//         required
-//       ></textarea>
-//     </div>
-//     <Button
-//       type="submit"
-//       isExpanded={true}
-//       text="Create New Allowlist"
-//       active={!loading}
-//       onClick={() => null}
-//     ></Button>
-//     {error && (
-//       <ErrorAlert
-//         title="Oops!"
-//         description={error}
-//         onClose={() => setError('')}
-//       />
-//     )}
-//   </form>
-// )
 
 export default function CreateAllowlist() {
   const auth = useAuth()
@@ -78,24 +13,16 @@ export default function CreateAllowlist() {
   const titleRef = React.createRef<HTMLInputElement>()
   const descriptionRef = React.createRef<HTMLInputElement>()
   const allowlistRef = React.createRef<HTMLTextAreaElement>()
+  const allowlistService = new AllowlistService()
 
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
-  const listsCollectionRef = collection(db, 'lists')
-
-  const isValidAddress = (adr: string) => {
-    try {
-      return ethers.utils.isAddress(adr)
-    } catch (e) {
-      return false
-    }
-  }
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     try {
       setLoading(true)
-const response = await allowlistService.create(
+      const response = await allowlistService.create(
         allowlistRef.current?.value ?? '',
         titleRef.current?.value ?? '',
         descriptionRef.current?.value ?? '',
@@ -105,7 +32,7 @@ const response = await allowlistService.create(
       if (!response?.success) {
         throw Error(response?.message)
       } else {
-        router.push('/creator'}
+        router.push('/creator')
       }
     } catch (error) {
       console.log(error)
