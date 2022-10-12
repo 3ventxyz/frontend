ec26ff07fe1e173c6c6d339cdc722e1ae5eb5fc3
constanzadl <constanzadl94@gmail.com>
2022-10-12 10:08:22 -0500

    verify fil

--- components/verify.tsx
+++ components/verify.tsx
@@ -48,6 +48,7 @@ export default function Verify() {
     getSize()
     setAddTwitterButtons(false)
   }, [addTwitterButtons])
+  
 
   const addTwAccount = async () => {
     setAddTwitterButtons(true)
