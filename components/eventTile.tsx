3fab92fc72eed167ebac9ca59da0cb158cf5df35
Locke <benkim96@gmail.com>
2022-10-12 13:37:28 -0700

    update event tile for user name and profile picture

--- components/eventTile.tsx
+++ components/eventTile.tsx
@@ -3,13 +3,33 @@ import Link from 'next/link'
 import { TbPhotoOff, TbPhoto } from 'react-icons/tb'
 import { EventInterface } from '../shared/interface/common'
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { doc, getDoc } from '@firebase/firestore'
+import { db } from '../services/firebase_config'
 
 export default function EventTile({
   eventData
 }: {
   eventData: EventInterface | null
 }) {
+  const [profileUrlImg, setProfileUrlImg] = useState('')
+  const [hostName, setHostName] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(db, 'users', eventData?.uid || '')
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        setProfileUrlImg(`${docSnap.data().avatar}`)
+        setHostName(docSnap.data().username)
+      }
+    }
+    if (eventData?.uid) {
+      fetchData()
+    }
+  }, [eventData])
+
   useEffect
   return !eventData ? (
     <div className=" mx-auto h-[460px] w-full max-w-[320px] animate-pulse cursor-pointer rounded-3xl bg-white sm:h-[524px] sm:w-[380px] sm:max-w-[380px]">
@@ -43,6 +63,23 @@ export default function EventTile({
               className="rounded-3xl"
             />
           )}
+          <Link href={`/u/${eventData.uid}`}>
+            <div className="absolute bottom-4 right-4 flex h-fit w-fit cursor-pointer flex-row items-center space-x-2 rounded-[14px] bg-white px-2 py-2">
+              {profileUrlImg ? (
+                <Image
+                  src={profileUrlImg}
+                  layout="fixed"
+                  width="35px"
+                  height="35px"
+                  loading="lazy"
+                  className="rounded-full bg-gray-200"
+                />
+              ) : (
+                <div className="h-[35px] w-[35px] rounded-full bg-gray-200" />
+              )}
+              <p className="">{hostName}</p>
+            </div>
+          </Link>
         </div>
         <ul className="p-[20px]">
           <li className="... truncate text-[24px] font-bold">
