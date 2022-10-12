750287c79ec431658165a1ee7e069c27d3f130cd
constanzadl <constanzadl94@gmail.com>
2022-10-12 11:05:57 -0500

    allowlist create

--- pages/allowlists/create.tsx
+++ pages/allowlists/create.tsx
@@ -6,6 +6,7 @@ import React, { FormEvent, useState } from 'react'
 import AllowlistService from '../../services/allowlists'
 import Button from '../../components/button'
 import ErrorAlert from '../../components/alerts/errorAlert'
+import TextInput from '../../components/textInput'
 
 export default function CreateAllowlist() {
   const auth = useAuth()
@@ -48,57 +49,15 @@ export default function CreateAllowlist() {
           Allowlist
         </h3>
         <form className="m-4 w-full" onSubmit={handleSubmit}>
+
           <div className="mb-6">
-            <label
-              htmlFor="title"
-              className="mb-2 block text-sm font-medium text-gray-900 "
-            >
-              TITLE
-            </label>
-            <input
-              type="text"
-              id="title"
-              ref={titleRef}
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-              required
-            />
+            <Button
+              type="submit"
+              text="Create"
+              active={!loading}
+              onClick={() => null}
+            ></Button>
           </div>
-          <div className="mb-6">
-            <label
-              htmlFor="description"
-              className="mb-2 block text-sm font-medium text-gray-900 "
-            >
-              DESCRIPTION
-            </label>
-            <input
-              type="text"
-              id="description"
-              ref={descriptionRef}
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className="mb-2 block text-sm font-medium text-gray-900"
-            >
-              ADDRESSES
-            </label>
-            <textarea
-              id="message"
-              rows={4}
-              ref={allowlistRef}
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-              placeholder="Add the list of addresses separated with comma"
-              required
-            ></textarea>
-          </div>
-          <Button
-            type="submit"
-            text="Create"
-            active={!loading}
-            onClick={() => null}
-          ></Button>
           {error && (
             <ErrorAlert
               title="Oops!"
