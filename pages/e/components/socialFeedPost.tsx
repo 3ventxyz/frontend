953c27f17069ee66df738e27222c99eb50d12d67
marrodri <laza.mar64@gmail.com>
2022-11-01 14:37:12 -0700

    SocialFeedPost and RegisteredAttendee template components added

---
+++ pages/e/components/socialFeedPost.tsx
@@ -0,0 +1,11 @@
+export default function SocialFeedPost() {
+  return (
+    <div className="flex items-center space-x-2 bg-yellow-200 px-[5px]">
+      <div className="h-[70px] w-[70px] rounded-full bg-green-100"></div>
+      <div>
+        <div>username commented:</div>
+        <div>comments from the user</div>
+      </div>
+    </div>
+  )
+}
