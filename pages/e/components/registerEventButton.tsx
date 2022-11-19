2ba9efa66d044c2fe673596ee1bbbd0defa9ceb6
marrodri <laza.mar64@gmail.com>
2022-11-18 16:42:18 -0800

    registerEventButton style fix

--- pages/e/components/registerEventButton.tsx
+++ pages/e/components/registerEventButton.tsx
@@ -2,7 +2,7 @@ export default function RegisterEventButton() {
   return (
     <div
       id="register-event-button"
-      className="flex h-[85px] items-center justify-center rounded-2xl bg-[#DE6767] bg-white shadow-md transition-shadow hover:cursor-pointer hover:shadow-xl"
+      className="flex h-[85px] items-center justify-center rounded-2xl bg-[#DE6767] shadow-md transition-shadow hover:cursor-pointer hover:shadow-xl"
     >
       <div className="text-[20px] font-bold text-white">Register Event</div>
     </div>
