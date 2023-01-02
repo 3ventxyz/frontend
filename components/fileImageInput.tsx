ef8807f101250c5310c7b991374883aa28c22c9e
marrodri <laza.mar64@gmail.com>
2023-01-02 15:16:46 -0800

    file image input component breakdowned functions; edit page attributes updated based on the new interface

--- components/fileImageInput.tsx
+++ components/fileImageInput.tsx
@@ -40,34 +40,14 @@ export default function FileImageInput({
             : 'h-[285px] w-[600px]'
         } relative rounded-3xl bg-gray-300 hover:cursor-pointer hover:bg-gray-500 sm:max-h-full`}
       >
-        <label htmlFor={`img-input-${mode}`}>
-          <Image
-            src={imgUrlTemplate}
-            layout="fill"
-            objectFit="cover"
-            className="rounded-3xl"
-          />
-          {isMouseHover ? (
-            <div className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-3xl bg-gray-400 text-white opacity-70">
-              <MdOutlineAddPhotoAlternate className="h-[150px] w-[150px]" />
-              <div>Please click to change your image</div>
-            </div>
-          ) : (
-            <></>
-          )}
-
-          <input
-            id={mode}
-            type="file"
-            className="hidden"
-            accept="image/*"
-            onChange={(event: any) => {
-              setFileImg(event.target.files[0])
-              setImgUrl(URL.createObjectURL(event.target.files[0]))
-              setMouseHover(false)
-            }}
-          />
-        </label>
+        <DisplayPredefinedImage
+          mode={mode}
+          imgUrlTemplate={imgUrlTemplate}
+          isMouseHover={isMouseHover}
+          setFileImg={setFileImg}
+          setImgUrl={setImgUrl}
+          setMouseHover={setMouseHover}
+        />
       </div>
     )
   }
@@ -87,33 +67,14 @@ export default function FileImageInput({
           : 'h-[285px] w-[600px]'
       } relative rounded-3xl bg-gray-300 hover:cursor-pointer hover:bg-gray-500 sm:max-h-full`}
     >
-      <label htmlFor={`img-input-${mode}`}>
-        <Image
-          src={imgUrl}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-3xl"
-        />
-        {isMouseHover ? (
-          <div className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-3xl bg-gray-400 text-white opacity-70">
-            <MdOutlineAddPhotoAlternate className="h-[150px] w-[150px]" />
-            <div>Please click to change your image</div>
-          </div>
-        ) : (
-          <></>
-        )}
-        <input
-          id={`img-input-${mode}`}
-          type="file"
-          className="hidden"
-          accept="image/*"
-          onChange={(event: any) => {
-            setFileImg(event.target.files[0])
-            setImgUrl(URL.createObjectURL(event.target.files[0]))
-            setMouseHover(false)
-          }}
-        />
-      </label>
+      <FileImageUploaded
+        mode={mode}
+        imgUrl={imgUrl}
+        isMouseHover={isMouseHover}
+        setFileImg={setFileImg}
+        setImgUrl={setImgUrl}
+        setMouseHover={setMouseHover}
+      />
     </div>
   ) : (
     // upload an image
@@ -122,32 +83,141 @@ export default function FileImageInput({
         mode === 'event'
           ? 'h-[384px] max-h-[320px] w-[380px]'
           : 'h-[285px] w-[600px]'
-      } relative rounded-3xl bg-gray-300 hover:bg-gray-400 sm:max-h-full`}
+      } relative rounded-3xl bg-gray-300 hover:cursor-pointer hover:bg-gray-400 sm:max-h-full`}
     >
-      <label htmlFor={`img-input-${mode}`}>
-        <input
-          id={`img-input-${mode}`}
-          type="file"
-          className="hidden"
-          accept="image/*"
-          onChange={(event: any) => {
-            setFileImg(event.target.files[0])
-            setImgUrl(URL.createObjectURL(event.target.files[0]))
-          }}
-        />
-        <div className="flex h-full w-full cursor-pointer flex-col items-center justify-center text-gray-400 hover:text-gray-500">
-          {mode === 'event' ? (
-            <MdOutlineAddPhotoAlternate className="h-[150px] w-[150px]" />
-          ) : (
-            <BiLandscape className="h-[150px] w-[150px]" />
-          )}
-          <div>
-            {mode === 'event'
-              ? 'Please add an Image'
-              : 'Please add a Landing Portrait'}
-          </div>
-        </div>
-      </label>
+      <UploadFileImage
+        mode={mode}
+        setFileImg={setFileImg}
+        setImgUrl={setImgUrl}
+      />
     </div>
+  )
+}
+
+function UploadFileImage({
+  mode,
+  setFileImg,
+  setImgUrl
+}: {
+  mode: string
+  setFileImg: (file: File) => void
+  setImgUrl: (url: string) => void
+}) {
+  return (
+    <label htmlFor={`img-input-${mode}`}>
+      <input
+        id={`img-input-${mode}`}
+        type="file"
+        className="hidden"
+        accept="image/*"
+        onChange={(event: any) => {
+          setFileImg(event.target.files[0])
+          setImgUrl(URL.createObjectURL(event.target.files[0]))
+        }}
+      />
+      <div className="flex h-full w-full cursor-pointer flex-col items-center justify-center text-gray-400 hover:text-gray-500">
+        {mode === 'event' ? (
+          <MdOutlineAddPhotoAlternate className="h-[150px] w-[150px]" />
+        ) : (
+          <BiLandscape className="h-[150px] w-[150px]" />
+        )}
+        <div>
+          {mode === 'event'
+            ? 'Please add an Image'
+            : 'Please add a Landing Portrait'}
+        </div>
+      </div>
+    </label>
+  )
+}
+
+function FileImageUploaded({
+  mode,
+  imgUrl,
+  isMouseHover,
+  setFileImg,
+  setImgUrl,
+  setMouseHover
+}: {
+  mode: string
+  imgUrl: string
+  isMouseHover: boolean
+  setFileImg: (file: File) => void
+  setImgUrl: (url: string) => void
+  setMouseHover: (mouseHover: boolean) => void
+}) {
+  return (
+    <label htmlFor={`img-input-${mode}`}>
+      <Image
+        src={imgUrl}
+        layout="fill"
+        objectFit="cover"
+        className="rounded-3xl"
+      />
+      {isMouseHover ? (
+        <div className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-3xl bg-gray-400 text-white opacity-70">
+          <MdOutlineAddPhotoAlternate className="h-[150px] w-[150px]" />
+          <div>Please click to change your image</div>
+        </div>
+      ) : (
+        <></>
+      )}
+      <input
+        id={`img-input-${mode}`}
+        type="file"
+        className="hidden"
+        accept="image/*"
+        onChange={(event: any) => {
+          setFileImg(event.target.files[0])
+          setImgUrl(URL.createObjectURL(event.target.files[0]))
+          setMouseHover(false)
+        }}
+      />
+    </label>
+  )
+}
+function DisplayPredefinedImage({
+  mode,
+  imgUrlTemplate,
+  isMouseHover,
+  setFileImg,
+  setImgUrl,
+  setMouseHover
+}: {
+  mode: string
+  imgUrlTemplate: string
+  isMouseHover: boolean
+  setFileImg: (file: File) => void
+  setImgUrl: (url: string) => void
+  setMouseHover: (mouseHover: boolean) => void
+}) {
+  return (
+    <label htmlFor={`img-input-${mode}`}>
+      <Image
+        src={imgUrlTemplate}
+        layout="fill"
+        objectFit="cover"
+        className="rounded-3xl"
+      />
+      {isMouseHover ? (
+        <div className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-3xl bg-gray-400 text-white opacity-70">
+          <MdOutlineAddPhotoAlternate className="h-[150px] w-[150px]" />
+          <div>Please click to change your image</div>
+        </div>
+      ) : (
+        <></>
+      )}
+      <input
+        id={mode}
+        type="file"
+        className="hidden"
+        accept="image/*"
+        onChange={(event: any) => {
+          setFileImg(event.target.files[0])
+          setImgUrl(URL.createObjectURL(event.target.files[0]))
+          setMouseHover(false)
+        }}
+      />
+    </label>
   )
 }
