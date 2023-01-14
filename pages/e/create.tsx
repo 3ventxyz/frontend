3a447f15d3db067ab08f94cbab87c62e4d5f2ecc
marrodri <laza.mar64@gmail.com>
2023-01-13 16:32:38 -0800

    useCreateEventForm function defined; hook,interface and initialInputValues migration

--- pages/e/create.tsx
+++ pages/e/create.tsx
@@ -20,41 +20,6 @@ import setFiletype from '../../shared/utils/setFileType'
 import NumberInput from '../../components/inputs/numberInput'
 import Spinner from '../../components/utils/spinner'
 
-interface createEventFormInterface {
-  title: string
-  start_date: Date
-  end_date: Date
-  event_location: LocationData
-  event_id: string
-  event_description: string
-  ticket_max: number
-  file_img: File | null
-  event_img_url: string
-  selected_predefined_event_img_url: string
-  landing_file_img: File | null
-  landing_img_url: string
-  selected_predefined_landing_img_url: string
-}
-
-const initialValues: createEventFormInterface = {
-  title: '',
-  start_date: startOfToday(),
-  end_date: startOfToday(),
-  event_location: {
-    address: '',
-    lat: 0,
-    long: 0
-  },
-  event_id: '',
-  event_description: '',
-  ticket_max: 0,
-  file_img: null,
-  selected_predefined_event_img_url: '',
-  landing_file_img: null,
-  selected_predefined_landing_img_url: '',
-  event_img_url: '',
-  landing_img_url: ''
-}
 
 export default function CreateEvent() {
   const router = useRouter()
@@ -67,7 +32,7 @@ export default function CreateEvent() {
   /**
    * input data UI setStates
    **/
-  const [values, setValues] = useState(initialValues);
+  // const [values, setValues] = useState(initialValues);
   const [title, setTitle] = useState<string>('')
   const [startDate, setStartDate] = useState<Date>(today)
   const [endDate, setEndDate] = useState<Date>(today)
