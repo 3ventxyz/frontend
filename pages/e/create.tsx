2b0dde818ff270f2dd885f8347ebc974a782d459
marrodri <laza.mar64@gmail.com>
2023-01-18 15:39:33 -0800

    CreateEventImageInput implemented in create.tsx file and code cleanup

--- pages/e/create.tsx
+++ pages/e/create.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 import { useAuth } from '../../contexts/auth'
 import 'react-datepicker/dist/react-datepicker.css'
@@ -7,12 +7,8 @@ import {
   createEventFormInterface,
   createEventStatusInterface
 } from '../../shared/interface/common'
-import LocalDatePicker from './components/datepicker'
-import LocalTimePicker from './components/timepicker'
 import EventLocationMap from './components/eventLocationMap'
 import CreateEventStepsDisplay from './components/createEventStepsDisplay'
-import FileImageInput from '../../components/inputs/fileImageInput'
-import PredefinedEventPictures from './components/predefinedEventPictures'
 import { useEvents } from '../../contexts/events'
 import { uploadImageToStorage } from '../../services/upload_image_to_storage'
 import CheckEventId from '../../services/check_event_id'
@@ -25,6 +21,7 @@ import useCreateEventValues from './hooks/create/useCreateEventValues'
 import { CreateEventFooter } from './components/createEventFooter'
 import CreateEventFormSection from './components/createEventFormSection'
 import CreateEventDateTimePicker from './components/createEventDateTimePicker'
+import CreateEventImageInput from './components/createEventImageInput'
 
 const inputValues: createEventFormInterface = {
   title: '',
@@ -71,27 +68,10 @@ export default function CreateEvent() {
    **/
   const [status, { nextPage, prevPage, setCreatingNewEvent, setErrorMsg, setCurrentStep }] =
     useCreateEventStatus(createEventStatus)
-  const [ticketImgsMenuVisible, setTicketImgsMenuVisible] = useState(true)
-  const [landingImgsMenuVisible, setLandingImgsMenuVisible] = useState(true)
 
   /**
    * logic functions
    **/
-  const onChangePredefinedImage = ({
-    setPredefinedImgUrl,
-    name,
-    imgUrl,
-    setMenuVisibility
-  }: {
-    setPredefinedImgUrl: (name: string, imgUrl: string) => void
-    imgUrl: string
-    name: string
-    setMenuVisibility: (visibility: boolean) => void
-  }) => {
-    setPredefinedImgUrl(name, imgUrl)
-    setMenuVisibility(false)
-  }
-
   const createEvent = async () => {
     let isFormValid
     setCreatingNewEvent(true)
@@ -270,92 +250,38 @@ export default function CreateEvent() {
               fatherClassName={'h-full space-y-[11px] md:h-[800px]'}
               childrenClassName={'my-[10px] md:items-start'}
             >
-              <div>
-                <div className="mx-auto flex w-full max-w-[400px] flex-col items-start text-[16px] font-normal">
-                  <div className="flex w-full justify-between">
-                    <label className="mb-2 block text-sm font-medium text-gray-900 ">
-                      TICKET EVENT IMAGE
-                    </label>
-                    <span
-                      onClick={() => {
-                        setTicketImgsMenuVisible(!ticketImgsMenuVisible)
-                      }}
-                      className="text-blue-800 hover:cursor-pointer hover:underline"
-                    >
-                      Predefined Images
-                    </span>
-                  </div>
-                  <div className="space-y-[5px]">
-                    <div className="z-10">
-                      <FileImageInput
-                        name={'file_img'}
-                        fileImg={values.file_img}
-                        setFileImg={setFileImg}
-                        imgUrlTemplate={values.event_img_url ?? ''}
-                      />
-                    </div>
-                    {values.file_img === null && ticketImgsMenuVisible ? (
-                      <div className="top-[345px] z-20 md:absolute md:py-[40px] md:px-[30px]">
-                        <PredefinedEventPictures
-                          setSelectedPredefinedEventImgUrl={(
-                            imgUrl: string
-                          ) => {
-                            onChangePredefinedImage({
-                              name: 'event_img_url',
-                              imgUrl: imgUrl,
-                              setPredefinedImgUrl: setPredefinedImgUrl,
-                              setMenuVisibility: setTicketImgsMenuVisible
-                            })
-                          }}
-                        />
-                      </div>
-                    ) : (
-                      <></>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="top-[780px] z-20 flex flex-col space-y-1 md:absolute md:w-[600px] md:space-y-0">
-                <div className="flex justify-between">
-                  <label className="block text-sm font-medium text-gray-900 md:mb-2 ">
-                    LANDING PORTRAIT IMAGES
-                  </label>
-                  <span
-                    onClick={() => {
-                      setLandingImgsMenuVisible(!landingImgsMenuVisible)
-                    }}
-                    className="text-blue-800 hover:cursor-pointer hover:underline"
-                  >
-                    Predefined Images
-                  </span>
-                </div>
-                <div className="z-10">
-                  <FileImageInput
-                    fileImg={values.landing_file_img}
-                    name={'landing_file_img'}
-                    setFileImg={setFileImg}
-                    imgUrlTemplate={values.landing_img_url ?? ''}
-                    mode={'landing'}
-                  />
-                </div>
-                {landingImgsMenuVisible ? (
-                  <div className="z-20 px-[15px] md:absolute md:top-[42px]">
-                    <PredefinedEventPictures
-                      setSelectedPredefinedEventImgUrl={(imgUrl: string) => {
-                        onChangePredefinedImage({
-                          imgUrl: imgUrl,
-                          name: 'landing_img_url',
-                          setPredefinedImgUrl: setPredefinedImgUrl,
-                          setMenuVisibility: setLandingImgsMenuVisible
-                        })
-                      }}
-                      landingMode={true}
-                    />
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </div>
+              <CreateEventImageInput
+                labelText={'TICKET EVENT IMAGE'}
+                fileImgName={'file_img'}
+                predefinedImgName={'event_img_url'}
+                fileImg={values.file_img}
+                setFileImg={setFileImg}
+                setPredefinedImgUrl={setPredefinedImgUrl}
+                eventImgUrl={values.event_img_url}
+                parentClassName={
+                  'mx-auto flex w-full max-w-[400px] flex-col items-start text-[16px] font-normal'
+                }
+                imageInputMenuClasses={
+                  'top-[345px] z-20 md:absolute md:py-[40px] md:px-[30px]'
+                }
+                landingMode={false}
+              />
+              <CreateEventImageInput
+                labelText={'LANDING PORTRAIT IMAGES'}
+                fileImgName={'landing_file_img'}
+                predefinedImgName={'landing_img_url'}
+                fileImg={values.landing_file_img}
+                setFileImg={setFileImg}
+                setPredefinedImgUrl={setPredefinedImgUrl}
+                eventImgUrl={values.landing_img_url}
+                parentClassName={
+                  'top-[780px] z-20 flex flex-col space-y-1 md:absolute md:w-[600px] md:space-y-0'
+                }
+                imageInputMenuClasses={
+                  'z-20 px-[15px] md:absolute md:top-[42px]'
+                }
+                landingMode={true}
+              />
             </CreateEventFormSection>
           </div>
           <CreateEventStepsDisplay 
@@ -364,7 +290,6 @@ export default function CreateEvent() {
           setCurrentStep={setCurrentStep}/>
         </div>
       </div>
-
       <CreateEventFooter
         currentStep={status.currentStep}
         isCreatingNewEvent={status.isCreatingNewEvent}
