1387c983d394b711bd122c3d3d3f932ad97cc6f1
marrodri <laza.mar64@gmail.com>
2023-01-19 09:38:41 -0800

    new components added for the create event page

--- components/inputs/numberInput.tsx
+++ components/inputs/numberInput.tsx
@@ -1,27 +1,34 @@
 export default function NumberInput({
   setNumberValue,
   name,
+  labelText,
   disabled = false
 }: {
   setNumberValue: (name: string, value: number) => void
   name: string
+  labelText:string
   disabled?: boolean
 }) {
   return (
-    <input
-      onChange={(e) => {
-        setNumberValue(name, parseInt(e.target.value))
-      }}
-      className={`focus:shadow-outline leading-0 h-full w-full max-w-[400px] rounded-lg border-[1.5px] p-2.5 text-sm ${
-        disabled
-          ? 'border-gray-300  text-gray-300'
-          : 'bg-gray-50   text-gray-700'
-      } px-2  focus:border-blue-500 focus:outline-none focus:ring-blue-500`}
-      id={'event_ticket_max'}
-      type="text"
-      placeholder={'0'}
-      disabled={disabled}
-    />
+    <div className="mx-auto flex w-full max-w-[400px] flex-col items-start space-y-1 text-[16px] font-normal">
+      <label className="mb-2 block text-sm font-medium text-gray-900 ">
+        {labelText}
+      </label>
+      <input
+        onChange={(e) => {
+          setNumberValue(name, parseInt(e.target.value))
+        }}
+        className={`focus:shadow-outline leading-0 h-full w-full max-w-[400px] rounded-lg border-[1.5px] p-2.5 text-sm ${
+          disabled
+            ? 'border-gray-300  text-gray-300'
+            : 'bg-gray-50   text-gray-700'
+        } px-2  focus:border-blue-500 focus:outline-none focus:ring-blue-500`}
+        id={'event_ticket_max'}
+        type="number"
+        placeholder={'0'}
+        disabled={disabled}
+      />
+    </div>
   )
 }
 
---
+++ pages/e/components/createEventImageInput.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import FileImageInput from '../../../components/inputs/fileImageInput'
+import PredefinedEventPictures from './predefinedEventPictures'
+
+export default function CreateEventImageInput({
+  labelText,
+  fileImg,
+  imgUrl,
+  parentClassName = '',
+  imgMenuClassName = '',
+  landingMode,
+  setFileImg,
+  setPredefinedImgUrl
+}: {
+  labelText: string
+  fileImg: File | null
+  imgUrl: string
+  parentClassName: string
+  imgMenuClassName: string
+  landingMode: boolean
+  setFileImg: (name: string, fileImg: File) => void
+  setPredefinedImgUrl: (name: string, predefinedImgUrl: string) => void
+}) {
+  const [menuVisible, setMenuVisible] = useState(true)
+  const imgUrlName = landingMode ? 'landing_img_url' : 'event_img_url'
+  const fileImgName = landingMode ? 'landing_file_img' : 'event_file_img'
+
+  const onChangePredefinedImage = ({
+    setPredefinedImgUrl,
+    imgUrl
+  }: {
+    setPredefinedImgUrl: (name: string, imgUrl: string) => void
+    imgUrl: string
+  }) => {
+    setPredefinedImgUrl(imgUrlName, imgUrl)
+    setMenuVisible(false)
+  }
+
+  return (
+    <div className={parentClassName}>
+      <div className="flex w-full justify-between">
+        <label className="mb-2 block text-sm font-medium text-gray-900 ">
+          {labelText}
+        </label>
+        <span
+          onClick={() => {
+            setMenuVisible(!menuVisible)
+          }}
+          className="text-blue-800 hover:cursor-pointer hover:underline"
+        >
+          Predefined Images
+        </span>
+      </div>
+      <div className="z-10">
+        <FileImageInput
+          name={fileImgName}
+          fileImg={fileImg}
+          setFileImg={setFileImg}
+          imgUrlTemplate={imgUrl ?? ''}
+          mode={landingMode ? 'landing' : 'event'}
+        />
+      </div>
+      {fileImg === null && menuVisible ? (
+        <div className={imgMenuClassName}>
+          <PredefinedEventPictures
+            setSelectedPredefinedEventImgUrl={(imgUrl: string) => {
+              onChangePredefinedImage({
+                imgUrl: imgUrl,
+                setPredefinedImgUrl: setPredefinedImgUrl
+              })
+            }}
+            landingMode={landingMode}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  )
+}
