3c93c26e0d8ba1a15469ae71809ef5a554d71b9a
marrodri <laza.mar64@gmail.com>
2023-01-24 22:15:39 -0800

    const errorMsgs and consts event error fields added

---
+++ pages/e/utils/consts.tsx
@@ -0,0 +1,27 @@
+// Create event error messages.
+
+export const emptyTitleErrorMsg = 'title is empty'
+export const invalidFileTypeErrorMsg =
+  'Selected Image is invalid type. Please upload jpg or png image.'
+export const emptyImageErrorMsg =
+  'Image is invalid, please select an image for your event.'
+export const emptyEventLocationErrorMsg = 'event location is not selected'
+export const startDateBehindErrorMsg =
+  'start date cannot be a previous date from today'
+export const sameDatePeriodErrorMsg =
+  'start date and end date cannot have the same time period'
+export const endDateBehindErrorMsg =
+  'end date cannot be behind the start date schedule'
+export const invalidNumberErrorMsg = 'Please enter a valid number of tickets'
+export const lowCapNumberErrorMsg =
+  'event capacity cannot be lower than the previous capacity'
+export const eventIdTakenErrorMsg =
+  'Event ID: event id has been taken, please enter a different id'
+
+/**create event error field consts */
+export const eventTitleErrorField = 'Event Title'
+export const eventImageErrorField = 'Event Image'
+export const eventLocationErrorField = 'Location'
+export const dateErrorField = 'Start Date/End Date'
+export const ticketErrorField = 'Tickets'
+export const eventIDErrorField = 'event ID'
