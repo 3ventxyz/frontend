ffb8584f1b54fe6c487a21db36f3aac5c08cca97
marrodri <laza.mar64@gmail.com>
2023-01-25 16:38:02 -0800

    consts error messages update

--- pages/e/utils/consts.tsx
+++ pages/e/utils/consts.tsx
@@ -1,25 +1,24 @@
 // Create event error messages.
 
 export const ERROR_MESSAGE = {
-  emptyTitle: 'title is empty',
-  invalidFileType:
-    'Selected Image is invalid type. Please upload jpg or png image.',
-  emptyImage: 'Image is invalid, please select an image for your event.',
+  emptyField: 'input field is empty',
+  invalidFileType: 'invalid file; please select a jpg or png file type.',
+  emptyImage: 'empty image; please select a file or a predefined image.',
   emptyEventLocation: 'event location is not selected',
   startDateBehind: 'start date cannot be a previous date from today',
   sameDatePeriod: 'start date and end date cannot have the same time period',
   endDateBehind: 'end date cannot be behind the start date schedule',
   invalidNumber: 'Please enter a valid number of tickets',
   lowCapNumber: 'event capacity cannot be lower than the previous capacity',
-  eventIdTaken: 'Event ID: event id has been taken, please enter a different id'
+  eventIdTaken: 'this id has been taken, please enter a different id'
 }
 
 /**create event error field consts */
 export const ERROR_FIELD = {
-  eventTitle: 'Event Title',
-  eventImage: 'Event Image',
-  eventLocation: 'Location',
-  date: 'Start Date/,End Date',
-  ticket: 'Tickets',
+  eventTitle: 'TITLE',
+  eventImage: 'EVENT/LANDING IMAGE',
+  eventLocation: 'LOCATION',
+  date: 'START/END DATE',
+  ticket: 'TICKET SUPPLY',
   eventID: 'event ID'
 }
