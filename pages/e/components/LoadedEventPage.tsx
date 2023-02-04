09c79bb3d48fe3b5b822e8097a76f485f33deb38
marrodri <laza.mar64@gmail.com>
2023-02-04 14:33:46 -0800

    dateCard and locationCard moved to eventDetails; eventDetails sticked to the top of event page

--- pages/e/components/LoadedEventPage.tsx
+++ pages/e/components/LoadedEventPage.tsx
@@ -2,12 +2,16 @@ import LandingPortrait from './landingPortrait'
 import RegisteredAttendees from './registeredAttendees'
 import SocialFeed from './socialFeed'
 import { useRouter } from 'next/router'
-import LocationCard from './locationCard'
-import DateCard from './dateCard'
 import RegisterEventButton from './registerEventButton'
 import { useEvents } from '../../../contexts/events'
 import { useUsers } from '../../../contexts/users'
+<<<<<<< HEAD
 import { Button } from '../../../components/buttons/button'
+import { EventDetails } from './eventDetails'
+=======
+import Button from '../../../components/buttons/button'
+import { EventDetails } from './eventDetails'
+>>>>>>> b7d9db5 (dateCard and locationCard moved to eventDetails; eventDetails sticked to the top of event page)
 
 // useRef for passing the host data that is fetched from the eid page.
 export default function LoadedEventPage({
@@ -29,7 +33,7 @@ export default function LoadedEventPage({
           event={events?.accessedEventData}
         />
       </div>
-      <div className="flex space-x-[15px] ">
+      <div className="flex space-x-[15px]">
         <div id="first-col" className="flex w-[625px] flex-col space-y-[20px]">
           <div className="w-[600px]">
             <h3>Event Description</h3>
@@ -45,37 +49,31 @@ export default function LoadedEventPage({
             eventData={events.accessedEventData}
           />
         </div>
-        <div id="second-col" className="w-[330px] space-y-5 ">
-          {/* TODO(2/2/2023) Move this to Event details component, and make these
-          2 components appear as a one merged UI component in the event page. */}
-          {/* Event Details Title Header */}
-          <div className="rounded-2xl bg-white ">
-            <div className="text-[28px] font-bold ">Event Details</div>
-            <hr />
-            <LocationCard event={events?.accessedEventData} />
-            <hr />
-            <DateCard event={events?.accessedEventData} />
-          </div>
-          {/* ================================================== */}
-
-          {/* TODO(2/2/2023) Marthel: Update this button to have a modal component. That it can help
+        <div id="second-col" className="w-[330px]">
+          <div className="sticky top-[100px] space-y-5">
+            <EventDetails event={events?.accessedEventData} />
+            {/* TODO(2/2/2023) Marthel: Update this button to have a modal component. That it can help
           to update the logged in user to update his/her info to register. And add a special disabler, for the 
           host. */}
-          <RegisterEventButton setShowModal={setShowModal} />
-          {isEventCreator ? (
-            <div>
-              <Button
-                text={'edit event'}
-                active={true}
-                onClick={() => {
-                  router.push(`edit?eid=${events.accessedEventData?.event_id}`)
-                  console.log('redirecting user to edit event page')
-                }}
-              />
-            </div>
-          ) : (
-            <></>
-          )}
+            <RegisterEventButton setShowModal={setShowModal} />
+            {/* ================================================== */}
+            {isEventCreator ? (
+              <div>
+                <Button
+                  text={'edit event'}
+                  active={true}
+                  onClick={() => {
+                    router.push(
+                      `edit?eid=${events.accessedEventData?.event_id}`
+                    )
+                    console.log('redirecting user to edit event page')
+                  }}
+                />
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </div>
     </div>
---
+++ pages/e/components/eventDetails.tsx
@@ -0,0 +1,114 @@
+import { EventInterface } from '../../../shared/interface/common'
+import { TbMap } from 'react-icons/tb'
+import Image from 'next/image'
+import { BsCalendar3 } from 'react-icons/bs'
+
+export function EventDetails({ event }: { event: EventInterface | null }) {
+  return (
+    <div className="rounded-2xl bg-white ">
+      <div className="text-[28px] font-bold ">Date and place</div>
+      <hr />
+      <LocationCard event={event} />
+      <hr />
+      <DateCard event={event} />
+    </div>
+  )
+}
+
+/**TODO(2/2/2023) Marthel: make the component expandable, based from the length of the address.
+ * Based from the number of characters, including spaces, there's going to be an increase of heigth for
+ * the whole div.
+ */
+function LocationCard({ event }: { event: EventInterface | null }) {
+  return (
+    <div id="location-card" className="flex h-[150px] space-x-3  px-[10px]  ">
+      {event != null ? (
+        <div className="relative h-[150px] w-[150px] rounded-2xl ">
+          <a
+            rel="noreferrer noopener"
+            target="_blank"
+            href={`http://maps.google.com?q=${event?.location?.lat},${event?.location?.long}`}
+          >
+            <Image
+              src={`https://maps.googleapis.com/maps/api/staticmap?center=${event?.location?.lat},${event?.location?.long}&zoom=15&size=300x300&markers=size:mid%color:blue%7Clabel:E%7C${event?.location?.lat},${event?.location?.long}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}`}
+              layout="fill"
+              loading="lazy"
+              objectFit="cover"
+              className="rounded-[20px]"
+            />
+          </a>
+        </div>
+      ) : (
+        <div className="flex h-[150px] w-[150px] items-center justify-center rounded-2xl bg-green-200">
+          <TbMap className="h-[50px] w-[50px]" />
+        </div>
+      )}
+
+      <div id="location-text " className="flex w-[100px] flex-col items-center">
+        <div className="text-[24px] font-bold">location</div>
+        <div className="text-wrap">
+          {event !== null ? event?.location.address : '...'}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/**TODO(2/2/2023). Marthel: Merge with the details component. So the UI looks all together. */
+function DateCard({ event }: { event: EventInterface | null }) {
+  return !event ? (
+    <div
+      id="date-card"
+      className="flex h-[100px] space-x-1  bg-white px-[10px]  "
+    >
+      <div className="flex h-[100px] w-[60px] items-center justify-center">
+        <BsCalendar3 className="h-[60px] w-[60px]" />
+      </div>
+      <div id="date-text" className="flex flex-col justify-center">
+        <div className="text-[24px] font-bold ">Date and time:</div>
+        <div>
+          <div>{'...'}</div>
+          <div>{'...'}</div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div
+      id="date-card"
+      className="flex h-[100px] space-x-1 rounded-2xl bg-white px-[8px] shadow-md"
+    >
+      <div className="flex h-[100px] w-[60px] items-center justify-center">
+        <BsCalendar3 className="h-[60px] w-[60px]" />
+      </div>
+      <div id="date-text" className="flex flex-col justify-center">
+        <div className="text-[24px] font-bold ">Date and time:</div>
+        <div>
+          {event?.start_date.toLocaleString('en-US', {
+            weekday: 'short',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric'
+          }) +
+            ', ' +
+            event?.start_date.toLocaleTimeString('en-US', {
+              hour: '2-digit',
+              minute: '2-digit'
+            }) +
+            ' - ' +
+            event?.end_date.toLocaleString('en-US', {
+              weekday: 'short',
+              year: 'numeric',
+              month: 'short',
+              day: 'numeric'
+            }) +
+            ', ' +
+            event?.end_date.toLocaleTimeString('en-US', {
+              hour: '2-digit',
+              minute: '2-digit',
+              timeZoneName: 'short'
+            })}
+        </div>
+      </div>
+    </div>
+  )
+}
