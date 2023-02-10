2de9e6283936bc2590db91cce622b6c2278b5dd6
marrodri <laza.mar64@gmail.com>
2023-02-09 19:37:02 -0800

    locationInput story bugfix; preview-head.html file added

--- stories/inputs/locationInput.stories.tsx
+++ stories/inputs/locationInput.stories.tsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { LocationInput } from '../../components/inputs/locationInput'
-
-const childrenTest = () => {
-  return <div className="bg-red-300">hello world</div>
-}
 
 export default {
   title: 'Inputs/LocationInput',
   component: LocationInput
 } as ComponentMeta<typeof LocationInput>
 
-const Template: ComponentStory<typeof LocationInput> = (args) => <LocationInput {...args} />
+const Template: ComponentStory<typeof LocationInput> = (args) => {
+  const [location, setLocation] = useState<any>(null)
+
+  return <LocationInput {...args} setLocation={setLocation} />
+}
 
 export const Primary = Template.bind({})
 
 Primary.args = {
-  children: childrenTest,
-  visible: true,
-  width: 'w-[200px]',
-  height: 'h-[200px]'
+  labelText: 'location title',
+  id: 'event-location',
+  placeholder: '123 street'
 }
