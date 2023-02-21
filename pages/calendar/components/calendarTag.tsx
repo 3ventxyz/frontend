f1cb669ee3fc2136d05f7b36dd23da2160767aba
marrodri <laza.mar64@gmail.com>
2023-02-20 19:26:00 -0800

    calendarTag added to the calendarDay component

--- pages/calendar/components/calendarTag.tsx
+++ pages/calendar/components/calendarTag.tsx
@@ -1,5 +1,40 @@
-export default function CalendarTag() {
+export default function CalendarTag({
+  date,
+  eventName,
+  num = 1
+}: {
+  date?: Date
+  eventName?: string
+  num?: number
+}) {
   // Build a calendar Tag system that is used for
   //adding a sticker on the calendar, and indicate the
   //user what happen or will hapens to the followed event.
+  switch (num) {
+    case 1:
+      return (
+        <div className="rounded-sm bg-[#FF9191] text-[12px] font-bold tracking-widest text-white shadow-sm ">
+          Full Event
+        </div>
+      )
+    case 2:
+      return (
+        <div className="rounded-sm bg-[#73ABFF] text-[12px] font-bold tracking-wide text-white shadow-sm hover:cursor-pointer hover:shadow-md">
+          Registered Event
+        </div>
+      )
+    case 3:
+      return (
+        <div className="rounded-sm bg-[#A1EE72] text-[12px] font-bold tracking-wide text-white shadow-sm hover:cursor-pointer hover:shadow-md">
+          Current Event
+        </div>
+      )
+
+    default:
+      return (
+        <div className="rounded-sm bg-black text-[12px] font-bold tracking-widest text-white shadow-sm hover:cursor-pointer hover:shadow-md">
+          Event No 5
+        </div>
+      )
+  }
 }
--- pages/calendar/components/mainCalendar.tsx
+++ pages/calendar/components/mainCalendar.tsx
@@ -1,7 +1,9 @@
+import CalendarTag from './calendarTag'
+
 export default function MainCalendar() {
   return (
     <div id="main-column" className="h-[750px] w-[850px] space-y-[3px]">
-      <div className="flex justify-evenly space-x-5 rounded-lg bg-black shadow-md py-[3px] text-[20px] font-bold text-white">
+      <div className="flex justify-evenly space-x-5 rounded-lg bg-black py-[3px] text-[20px] font-bold text-white shadow-md">
         <div>Sun</div>
         <div>Mon</div>
         <div>Tues</div>
@@ -28,24 +30,17 @@ function CalendarDay({
 }) {
   //TODO build a calendar day block, that
   //holds the day number, the events that will be stored.
+
   return (
     <div className="flex h-[125px] w-[120px] flex-col items-end space-y-[1px] rounded-2xl border bg-white shadow-md">
       <div className=" mr-[3px] mt-[3px] flex h-[25px] w-[25px] items-center justify-center rounded-3xl  bg-black text-[12px] font-bold text-white">
         <p>{dayNumber}</p>
       </div>
       <div id="tags" className="h-[85px] w-full space-y-[4px] px-[3px]">
-        <div className="rounded-sm bg-[#FF9191] text-[12px] font-bold tracking-widest text-white shadow-sm ">
-          Full Event
-        </div>
-        <div className="rounded-sm bg-black text-[12px] font-bold tracking-widest text-white shadow-sm hover:shadow-md hover:cursor-pointer">
-          Event No 5
-        </div>
-        <div className="rounded-sm bg-[#73ABFF] text-[12px] font-bold tracking-wide text-white shadow-sm hover:shadow-md hover:cursor-pointer">
-          Registered Event
-        </div>
-        <div className="rounded-sm bg-[#73ABFF] text-[12px] font-bold tracking-wide text-white shadow-sm hover:shadow-md hover:cursor-pointer">
-          Registered Event
-        </div>
+        <CalendarTag num={1} />
+        <CalendarTag num={2} />
+        <CalendarTag num={3} />
+        <CalendarTag num={0} />
       </div>
     </div>
   )
